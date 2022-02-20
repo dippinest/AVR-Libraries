54bac70e25da3a3b1ac3c7e6072fcd6d042e549c
Denis Chicherov <83783175+dippinest@users.noreply.github.com>
2022-02-20 18:06:29 +0300

    Add files via upload
    Added a library for converting integer types to BCD format numbers

---
+++ Crossplatform/numconvert/bcd/Examples/main.c
@@ -0,0 +1,21 @@
+#define  F_CPU 16000000UL
+#include <util/delay.h>
+
+#include "uart.h"
+#include "bcdcode.h"
+
+int main(void)
+{
+	UART_Initialize(9600, true, true, false, false, false, UART_NUM_OF_DATA_BITS_8, UART_NUM_OF_STOP_BITS_1, UART_PARITY_BIT_NONE);
+	
+	unsigned char i = 0, bcd;
+	
+	while (1)
+	{
+		bcd = BCDCODE_UInt8_To_UInt8BCD(i);
+		UART_StringFmt_Transmit("Counter: 0x%X (%d); BCD code: 0x%X; normal code: 0x%X\r\n", i, i, bcd, BCDCODE_UInt8BCD_To_UInt8(bcd));
+		++i;
+		
+		_delay_ms(200);
+	}
+}
