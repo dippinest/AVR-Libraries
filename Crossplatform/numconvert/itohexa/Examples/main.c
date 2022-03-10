04470789c33a438af869597c238e3310846c1446
Denis Chicherov <83783175+dippinest@users.noreply.github.com>
2022-03-10 07:15:42 +0300

    Update main.c

--- Crossplatform/numconvert/itohexa/Examples/main.c
+++ Crossplatform/numconvert/itohexa/Examples/main.c
@@ -1,8 +1,8 @@
 #define  F_CPU 16000000UL
 #include <util/delay.h>
 
-#include "uart.h"
-#include "itohexa.h"
+#include "AVR/Internal Peripherals/uart/uart.h"
+#include "Crossplatform/numconvert/itohexa/itohexa.h"
 
 static char str_buf[8];
 
