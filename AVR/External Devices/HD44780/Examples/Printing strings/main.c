f16d4f2f9bc198f8a63aaa3af211bfb177022c61
Denis Chicherov <83783175+dippinest@users.noreply.github.com>
2022-04-30 19:51:03 +0300

    Update main.c

--- AVR/External Devices/HD44780/Examples/Printing strings/main.c
+++ AVR/External Devices/HD44780/Examples/Printing strings/main.c
@@ -1,21 +1,3 @@
-#define HD44780_RS_PORT   PORTD
-#define HD44780_RS_PIN    0
-
-#define HD44780_E_PORT    PORTD
-#define HD44780_E_PIN     1
-
-#define HD44780_D4_PORT   PORTD
-#define HD44780_D4_PIN    2
-
-#define HD44780_D5_PORT   PORTD
-#define HD44780_D5_PIN    3
-
-#define HD44780_D6_PORT   PORTD
-#define HD44780_D6_PIN    4
-
-#define HD44780_D7_PORT   PORTD
-#define HD44780_D7_PIN    5
-
 #define F_CPU 16000000UL
 
 #include "hd44780.h"
