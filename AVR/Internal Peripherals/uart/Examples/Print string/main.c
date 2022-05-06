e8289da17590a581df9e0e3be24a8bd478da3f21
Denis Chicherov <83783175+dippinest@users.noreply.github.com>
2022-05-06 18:23:18 +0300

    Add files via upload

---
+++ AVR/Internal Peripherals/uart/Examples/Print string/main.c
@@ -0,0 +1,16 @@
+
+#include <stdbool.h>
+
+#define  F_CPU 16000000UL
+#include "uart.h"
+
+int main(void)
+{
+	UART_Initialize(9600, true, false);
+	
+	UART_String_Transmit("Hello from UART!");
+	
+	while (1)
+	{
+	}
+}
