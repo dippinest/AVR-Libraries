5bf185ab3134cf81181fdee8ba08d6cb834c75d6
Denis Chicherov <83783175+dippinest@users.noreply.github.com>
2022-05-07 10:36:09 +0300

    Add files via upload

---
+++ AVR/External Devices/HD44780_I2C/Examples/Print string format/main.c
@@ -0,0 +1,25 @@
+
+#define F_CPU 16000000UL
+
+#include "i2c.h"
+#include "hd44780_i2c.h"
+
+int main(void)
+{
+	I2C_Initialize(100000);
+	
+	HD44780_I2C display = HD44780_I2C_Get_Display_Object(HD44780_I2C_PCF8574_DEV_ADDR_A2T_A1T_A0T, true);
+	
+	HD44780_I2C_Set_Target_Display_Object(&display);
+	
+	HD44780_I2C_Set_Cursor_Pos(0, 0); HD44780_I2C_Print_String("Hello, world!");
+	HD44780_I2C_Set_Cursor_Pos(1, 0); HD44780_I2C_Print_String("Counter Fmt: ");
+	
+	uint8_t i = 0;
+	
+	while (1)
+	{
+		HD44780_I2C_Set_Cursor_Pos(1, 13); HD44780_I2C_Print_StringFmt("%d", i++);
+		_delay_ms(500);
+	}
+}
