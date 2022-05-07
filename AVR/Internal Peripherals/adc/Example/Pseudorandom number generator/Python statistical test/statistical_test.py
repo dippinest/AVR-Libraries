b3fc504aff83007bd91b41379f855f3bcaad44f5
Denis Chicherov <83783175+dippinest@users.noreply.github.com>
2022-05-07 14:22:53 +0300

    Update statistical_test.py

--- AVR/Internal Peripherals/adc/Example/Pseudorandom number generator/Python statistical test/statistical_test.py
+++ AVR/Internal Peripherals/adc/Example/Pseudorandom number generator/Python statistical test/statistical_test.py
@@ -4,14 +4,14 @@ import pandas            as pd
 import numpy             as np
 
 # this is a statistical test of the distribution of pseudorandom numbers
-# of the ADC_Get_Random_Entropy_Value_8 bit(channel) function. The test result
+# of the ADC_Get_Random_Entropy_Value_8bit(channel) function. The test result
 # is also valid for other similar functions from the adc.h library.
 #
 # 10,000 generated values were used for the test (in the data.csv file)
 #
 # The resulting distribution has pronounced peaks, so these functions cannot be
 # used for serious applications. I recommend using them in conjunction with the
-# stand() and rand() functions from the C standard library as the initialization seed
+# srand() and rand() functions from the C standard library as the initialization seed
 
 data = pd.read_csv('data.csv', sep=',', header=None).to_numpy()[0]
 
