b9842039d21169befb709fc6fb26e15905682213
Denis Chicherov <83783175+dippinest@users.noreply.github.com>
2022-05-09 20:14:33 +0300

    Update main.c

--- Crossplatform/numconvert/itoa/Examples/For AVR/Convert float numbers/main.c
+++ Crossplatform/numconvert/itoa/Examples/For AVR/Convert float numbers/main.c
@@ -3,9 +3,9 @@
 // when using the "ITOA_Float_To_String" function instead
 // of "printf" ("sprintf") type functions. In the first case,
 // with the value of the optimization flag -Os (optimization
-// by code size, gcc-avr compiler), the code volume is 2118 bytes,
+// by code size, gcc-avr compiler), the code volume is 2170 bytes,
 // while the similar code with the "sprintf" function takes 3540 bytes.
-// Memory savings are (3540 - 2118) = 1422 bytes
+// Memory savings are (3540 - 2170) = 1370 bytes
 
 #define F_CPU 16000000UL
 #include <util/delay.h>
