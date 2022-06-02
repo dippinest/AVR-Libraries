e6539e2d9a36505ec0e2bdbd97359419314e678d
Denis Chicherov <83783175+dippinest@users.noreply.github.com>
2022-06-02 15:30:52 +0300

    uart non-blocking

---
+++ AVR/Internal Peripherals/uart non-blocking/Examples/Flash strings/main.c
@@ -0,0 +1,20 @@
+
+
+#include "uart.h"
+#include "uart_non_blocking.h"
+
+const char _F[] PROGMEM = "ПРИВЕТ, МИР!";
+
+int main(void)
+{
+	UART_Initialize(9600, true, false);
+	
+	sei();
+	UART_NB_Set_Transmittion_Enable(true);
+	
+	UART_NB_Flash_StringLn_Transmit(_F);
+	
+	while (1)
+	{
+	}
+}
