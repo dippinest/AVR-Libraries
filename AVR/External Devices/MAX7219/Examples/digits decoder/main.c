e1b94101fa9a4fa27bc29a99aec12ae9a238d27e
Denis Chicherov <83783175+dippinest@users.noreply.github.com>
2022-06-06 14:17:32 +0300

    Update main.c

--- AVR/External Devices/MAX7219/Examples/digits decoder/main.c
+++ AVR/External Devices/MAX7219/Examples/digits decoder/main.c
@@ -28,7 +28,7 @@ int main(void)
 	DDRD |= (1 << 7);
 	
 	SPI_Initialize(true, SPI_DEV_IS_MASTER, SPI_MODE_0, SPI_SPEED_FCPU_DIV_2);
-	SPI_Set_CS_CallBacks_Function(CS_ON, CS_OFF);
+	SPI_Set_CS_CallBack_Functions(CS_ON, CS_OFF);
 	
 	MAX7219_Initialize(true, MAX7219_DECODE_MODE_DECODE_ALL_DIGITS, MAX7219_8_DIGIT, MAX7219_BRIGHTNESS_COEFFICIENT_4);
 	
