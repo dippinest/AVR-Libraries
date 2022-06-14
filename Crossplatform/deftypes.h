93a62b367e852a28f5e7a82bde5a07bd232fa677
Denis Chicherov <83783175+dippinest@users.noreply.github.com>
2022-06-14 13:59:21 +0300

    Update deftypes.h

--- Crossplatform/deftypes.h
+++ Crossplatform/deftypes.h
@@ -12,6 +12,7 @@ typedef int8_t     sbyte;
 typedef int8_t     int8;
 typedef uint8_t    uint8;
 typedef int8_t     s8;
+typedef int8_t     i8;
 typedef uint8_t    u8;
 typedef uint8_t    natural8;
 
@@ -21,6 +22,7 @@ typedef int16_t    sword;
 typedef int16_t    int16;
 typedef uint16_t   uint16;
 typedef int16_t    s16;
+typedef int16_t    i16;
 typedef uint16_t   u16;
 typedef uint16_t   natural16;
 
@@ -30,6 +32,7 @@ typedef int32_t    sdword;
 typedef int32_t    int32;
 typedef uint32_t   uint32;
 typedef int32_t    s32;
+typedef int32_t    i32;
 typedef uint32_t   u32;
 typedef uint32_t   natural32;
 
@@ -39,6 +42,7 @@ typedef int64_t    sqword;
 typedef int64_t    int64;
 typedef uint64_t   uint64;
 typedef int64_t    s64;
+typedef int64_t    i64;
 typedef uint64_t   u64;
 typedef uint64_t   natural64;
 
