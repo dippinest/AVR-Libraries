a35233922a306aa384050c9e4c07aa00d2302048
Denis Chicherov <dippinest.workmail@gmail.com>
2022-06-29 19:02:10 +0300

    Update main.c

--- AVR/Internal Peripherals/uart/Examples/Flash strings/main.c
+++ AVR/Internal Peripherals/uart/Examples/Flash strings/main.c
@@ -3,7 +3,6 @@
 
 #define F_CPU 16000000UL
 #include "uart.h"
-#include "deftypes.h"
 
 const char FLASH_STRING[] PROGMEM = "this string is in a flash memory";	
 const char FLASH_FMT_STRING[] PROGMEM = "Math constant Pi ~ %.2f\r\n";
@@ -13,7 +12,7 @@ int main(void)
 	UART_Initialize(9600, true, false);
 	
 	UART_Flash_StringLn_Transmit(FLASH_STRING);
-	UART_Flash_StringFmt_Transmit(FLASH_FMT_STRING, MATH_PI); // function not supported '%s' from flash!
+	UART_Flash_StringFmt_Transmit(FLASH_FMT_STRING, 3.14); // function not supported '%s' from flash!
 	
 	while (1)
 	{
