07d19851293024ed30ed78bd21c5fb19a148a0fd
Denis Chicherov <dippinest.workmail@gmail.com>
2022-07-12 07:57:38 +0300

    Update max6675_configuration.h

--- AVR/External Devices/MAX6675/max6675_configuration.h
+++ AVR/External Devices/MAX6675/max6675_configuration.h
@@ -2,8 +2,11 @@
 #ifndef MAX6675_CONFIGURATION_H_
 #define MAX6675_CONFIGURATION_H_
 
-// macro for allowing work with spi chip-select callback.
+// macro for using software SPI
+#define MAX6675_USE_SOFTSPI
+
+// macro for allowing work with SPI chip-select callback.
 // if you control CS output directly, comment out this macro
-#define MAX6675_USE_SPI_CS_CALLBACKS
+#define MAX6675_USE_CS_CALLBACKS
 
 #endif
