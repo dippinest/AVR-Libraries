229e50ea2c5eae5706ae87561e489af9ae8cc454
Denis Chicherov <dippinest.workmail@gmail.com>
2022-08-05 08:41:14 +0300

    Update itohexa.c

--- Crossplatform/numconvert/itohexa/itohexa.c
+++ Crossplatform/numconvert/itohexa/itohexa.c
@@ -3,6 +3,9 @@
 
 static char *_string_buffer = NULL; // buffer for string
 
+static const char _HEX_DIGIT_CHARS[] =
+	{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
 // ===============================================================================
 
 void ITOHEXA_Set_String_Buffer(char *string_buffer_ptr)
@@ -25,12 +28,9 @@ void ITOHEXA_Delete_String_Buffer_To_Dynamic_Memory()
 
 // ===============================================================================
 
-static char *_nbyte_num_to_hex_string(uint64_t val, bool is_lower_case, const uint8_t val_nbytes)
+char *ITOHEXA_8bitNum_To_HexString(uint8_t val, bool is_lower_case)
 {
-	static const char _HEX_DIGIT_CHARS[] =
-        { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-
-	for (int8_t i = (val_nbytes * 2 - 1); i >= 0; --i)
+	for (int8_t i = 1; i >= 0; --i)
 	{
 		if (is_lower_case)
 		{
@@ -44,27 +44,70 @@ static char *_nbyte_num_to_hex_string(uint64_t val, bool is_lower_case, const ui
 		val >>= 4;
 	}
 
-	_string_buffer[val_nbytes * 2] = '\0';
-
+	_string_buffer[2] = '\0';
+	
 	return _string_buffer;
-}
-
-char *ITOHEXA_8bitNum_To_HexString(uint8_t val, bool is_lower_case)
-{
-	return _nbyte_num_to_hex_string(val, is_lower_case, 1);
 }
 
 char *ITOHEXA_16bitNum_To_HexString(uint16_t val, bool is_lower_case)
 {
-	return _nbyte_num_to_hex_string(val, is_lower_case, 2);
+	for (int8_t i = 3; i >= 0; --i)
+	{
+		if (is_lower_case)
+		{
+			_string_buffer[i] = (char)tolower(_HEX_DIGIT_CHARS[val & 0x0F]);
+		}
+		else
+		{
+			_string_buffer[i] = _HEX_DIGIT_CHARS[val & 0x0F];
+		}
+
+		val >>= 4;
+	}
+
+	_string_buffer[4] = '\0';
+	
+	return _string_buffer;
 }
 
 char *ITOHEXA_32bitNum_To_HexString(uint32_t val, bool is_lower_case)
 {
-	return _nbyte_num_to_hex_string(val, is_lower_case, 4);
+	for (int8_t i = 7; i >= 0; --i)
+	{
+		if (is_lower_case)
+		{
+			_string_buffer[i] = (char)tolower(_HEX_DIGIT_CHARS[val & 0x0F]);
+		}
+		else
+		{
+			_string_buffer[i] = _HEX_DIGIT_CHARS[val & 0x0F];
+		}
+
+		val >>= 4;
+	}
+
+	_string_buffer[8] = '\0';
+	
+	return _string_buffer;
 }
 
 char *ITOHEXA_64bitNum_To_HexString(uint64_t val, bool is_lower_case)
 {
-	return _nbyte_num_to_hex_string(val, is_lower_case, 8);
+	for (int8_t i = 15; i >= 0; --i)
+	{
+		if (is_lower_case)
+		{
+			_string_buffer[i] = (char)tolower(_HEX_DIGIT_CHARS[val & 0x0F]);
+		}
+		else
+		{
+			_string_buffer[i] = _HEX_DIGIT_CHARS[val & 0x0F];
+		}
+
+		val >>= 4;
+	}
+
+	_string_buffer[16] = '\0';
+	
+	return _string_buffer;
 }
