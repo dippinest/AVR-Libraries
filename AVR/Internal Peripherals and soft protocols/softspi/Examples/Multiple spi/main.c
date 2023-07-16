c3e05f81bcc81ef198a419653f7f7efae2b9cae2
Denis Chicherov <dippinest.workmail@gmail.com>
2023-07-16 07:02:58 +0300

    Add files via upload

---
+++ AVR/Internal Peripherals and soft protocols/softspi/Examples/Multiple spi/main.c
@@ -0,0 +1,80 @@
+
+#include "softspi.h"
+
+// chip select ON for SPI1
+inline void CS_ON_1()
+{
+	PORTA &= ~(1 << 3);
+}
+
+// chip select OFF for SPI1
+inline void CS_OFF_1()
+{
+	PORTA |=  (1 << 3);
+}
+
+
+// chip select ON for SPI2
+inline void CS_ON_2()
+{
+	PORTD &= ~(1 << 7);
+}
+
+// chip select OFF for SPI2
+inline void CS_OFF_2()
+{
+	PORTD |=  (1 << 7);
+}
+
+uint8_t spi1_transmitted_data[] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}; // data to be sent via SPI1
+uint8_t spi2_transmitted_data[] = {0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7}; // data to be sent via SPI2
+
+int main(void)
+{
+	// SPI1
+	//
+	// MISO - PORTA 0
+	// MOSI - PORTA 1
+	// CLK  - PORTA 2
+	//
+	// SPI mode - MODE0
+	// Data order is highest bit
+	SOFTSPI_t spi_1 = SOFTSPI_Get_Interface_Object(_T(DDRA), _T(PINA), 0, _T(DDRA), _T(PORTA), 1, _T(DDRA), _T(PORTA), 2, SOFTSPI_MODE_0, SOFTSPI_DATA_IS_ORDER_HIGHEST_BIT);
+	
+	// SPI1
+	//
+	// MISO - PORTD 4
+	// MOSI - PORTD 5
+	// CLK  - PORTD 6
+	//
+	// SPI mode - MODE0
+	// Data order is highest bit
+	SOFTSPI_t spi_2 = SOFTSPI_Get_Interface_Object(_T(DDRD), _T(PIND), 4, _T(DDRD), _T(PORTD), 5, _T(DDRD), _T(PORTD), 6, SOFTSPI_MODE_0, SOFTSPI_DATA_IS_ORDER_HIGHEST_BIT);
+	
+	DDRA |= (1 << 3);
+	DDRD |= (1 << 7);
+	
+	
+	// setting SPI1
+	SOFTSPI_Set_Target_Interface_Object(&spi_1);
+	
+	CS_ON_1();
+	
+	SOFTSPI_Send_Data(spi1_transmitted_data, 8); // send data
+	
+	CS_OFF_1();
+	
+	
+	// setting SPI2
+	SOFTSPI_Set_Target_Interface_Object(&spi_2);
+	
+	CS_ON_2();
+	
+	SOFTSPI_Send_Data(spi2_transmitted_data, 8); // send data
+	
+	CS_OFF_2();
+	
+	while (1)
+	{
+	}
+}
