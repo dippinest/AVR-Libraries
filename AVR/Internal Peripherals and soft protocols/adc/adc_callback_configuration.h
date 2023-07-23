a2557b81571ce49644c1dbf699f0a1c8e6a62a10
Denis Chicherov <dippinest.workmail@gmail.com>
2023-07-23 18:52:17 +0300

    Delete AVR/Internal Peripherals and soft protocols/adc directory

--- AVR/Internal Peripherals and soft protocols/adc/adc_callback_configuration.h
+++
@@ -1,9 +0,0 @@
-#ifndef ADC_CALLBACK_CONFIGURATION_H_
-#define ADC_CALLBACK_CONFIGURATION_H_
-
-// if you don't want to use the built-in callback
-// to receive data, comment out #define
-
-#define ADC_USE_CALLBACK
-
-#endif
