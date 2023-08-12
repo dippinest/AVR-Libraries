9b70f150c761ea27e119de0e84b62155ebd2de44
Denis Chicherov <dippinest.workmail@gmail.com>
2023-08-12 18:10:08 +0300

    Add files via upload

---
+++ AVR/Internal Peripherals and soft protocols/adc/adc_configuration.h
@@ -0,0 +1,9 @@
+#ifndef ADC_CONFIGURATION_H_
+#define ADC_CONFIGURATION_H_
+
+// if you don't want to use the built-in callback
+// to receive data, comment out #define
+
+#define ADC_USE_CALLBACK
+
+#endif
