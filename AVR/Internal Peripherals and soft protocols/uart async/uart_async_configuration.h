851d273533003328dd41229ef233b9cf6bb9e2ba
Denis Chicherov <dippinest.workmail@gmail.com>
2023-09-03 18:30:54 +0300

    Update uart_async_configuration.h
    bugs fixed, comments added

--- AVR/Internal Peripherals and soft protocols/uart async/uart_async_configuration.h
+++ AVR/Internal Peripherals and soft protocols/uart async/uart_async_configuration.h
@@ -2,10 +2,20 @@
 #ifndef UART_ASYNC_CONFIGURATION_H_
 #define UART_ASYNC_CONFIGURATION_H_
 
-// if you don't want to use reception or
-// transmission, comment out the appropriate #define
 
-#define UART_ASYNC_USE_TX
+// если вы хотите активировать асинхронный приём данных по UART,
+// раскомментируйте этот дефайн (иначе закомментируйте его)
+// -------------------------------------------------------------------------------
+// if you want to activate asynchronous data reception via UART,
+// uncomment this define (otherwise comment it out)
 #define UART_ASYNC_USE_RX
+
+
+// если вы хотите активировать асинхронную передачу данных по UART,
+// раскомментируйте этот дефайн (иначе закомментируйте его)
+// -------------------------------------------------------------------------------
+// if you want to activate asynchronous data transfer via UART,
+// uncomment this define (otherwise comment it out)
+#define UART_ASYNC_USE_TX
 
 #endif
