7afd65379d0039cc31269358c2052e88b4c08725
Denis Chicherov <dippinest.workmail@gmail.com>
2023-10-01 15:34:45 +0300

    Update hd44780_configuration.h

--- AVR/External Devices/HD44780/hd44780_configuration.h
+++ AVR/External Devices/HD44780/hd44780_configuration.h
@@ -2,28 +2,60 @@
 #ifndef HD44780_CONFIGURATION_H_
 #define HD44780_CONFIGURATION_H_
 
+
+// определения GPIO для вывода RS LCD дисплея на HD44780
+// -------------------------------------------------------------------------------
+// GPIO definitions for RS LCD display output on HD44780
+
 #define HD44780_RS_DDR   DDRD
 #define HD44780_RS_PORT  PORTD
 #define HD44780_RS_PIN   0
+
+
+// определения GPIO для вывода E LCD дисплея на HD44780
+// -------------------------------------------------------------------------------
+// GPIO definitions for E LCD display output on HD44780
 
 #define HD44780_E_DDR    DDRD
 #define HD44780_E_PORT   PORTD
 #define HD44780_E_PIN    1
 
+
+// определения GPIO для вывода D4 LCD дисплея на HD44780
+// -------------------------------------------------------------------------------
+// GPIO definitions for D4 LCD display output on HD44780
+
 #define HD44780_D4_DDR   DDRD
 #define HD44780_D4_PORT  PORTD
 #define HD44780_D4_PIN   2
+
+
+// определения GPIO для вывода D5 LCD дисплея на HD44780
+// -------------------------------------------------------------------------------
+// GPIO definitions for D5 LCD display output on HD44780
 
 #define HD44780_D5_DDR   DDRD
 #define HD44780_D5_PORT  PORTD
 #define HD44780_D5_PIN   3
 
+
+// определения GPIO для вывода D6 LCD дисплея на HD44780
+// -------------------------------------------------------------------------------
+// GPIO definitions for D6 LCD display output on HD44780
+
 #define HD44780_D6_DDR   DDRD
 #define HD44780_D6_PORT  PORTD
 #define HD44780_D6_PIN   4
+
+
+// определения GPIO для вывода D7 LCD дисплея на HD44780
+// -------------------------------------------------------------------------------
+// GPIO definitions for D7 LCD display output on HD44780
 
 #define HD44780_D7_DDR   DDRD
 #define HD44780_D7_PORT  PORTD
 #define HD44780_D7_PIN   5
 
 #endif
+
+
