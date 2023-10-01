03bb891c5e1a6b0c89f0e9d64f20354611adaac9
Denis Chicherov <dippinest.workmail@gmail.com>
2023-10-01 15:47:14 +0300

    Update hd44780_i2c_configuration.h

--- AVR/External Devices/HD44780_I2C/hd44780_i2c_configuration.h
+++ AVR/External Devices/HD44780_I2C/hd44780_i2c_configuration.h
@@ -2,7 +2,15 @@
 #ifndef HD44780_I2C_CONFIGURATION_H_
 #define HD44780_I2C_CONFIGURATION_H_
 
-// macro for using software I2C
+
+// если вы хотите работать с устройством посредством программного I2C,
+// раскомментируйте этот дефайн (иначе закомментируйте его)
+// -------------------------------------------------------------------------------
+// if you want to work with the device via software I2C,
+// uncomment this define (otherwise comment it out)
+
 #define HD44780_I2C_USE_SOFTI2C
 
 #endif
+
+
