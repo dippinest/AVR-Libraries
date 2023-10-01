688b7ec0ecccd81dd276549e1f617d146f9d6b54
Denis Chicherov <dippinest.workmail@gmail.com>
2023-10-01 16:42:26 +0300

    Update main.c

--- AVR/External Devices/MAX7219/Examples/text/main.c
+++ AVR/External Devices/MAX7219/Examples/text/main.c
@@ -1,8 +1,6 @@
 
-// ATN! to run the example, be sure to uncomment out the
-// MAX7219_USE_SPI_CS_CALLBACKS macro in the max7219_configuration.h file!
 
-#include "spi.h"
+#include "softspi.h"
 #include "max7219.h"
 #include "max7219_symbols.h"
 
@@ -20,30 +18,30 @@ int main(void)
 {
 	DDRD |= (1 << 7);
 	
-	SPI_Initialize(true, SPI_DEV_IS_MASTER, SPI_MODE_0, SPI_SPEED_FCPU_DIV_2);
-	SPI_Set_CS_CallBack_Functions(CS_ON, CS_OFF);
+	SOFTSPI_Initialize();
+	SOFTSPI_Set_CS_CallBack_Functions(CS_ON, CS_OFF);
 	
 	MAX7219_Initialize(true, MAX7219_DECODE_MODE_NO_DECODE, MAX7219_8_DIGIT, MAX7219_BRIGHTNESS_COEFFICIENT_4);
 	
 	
 	// data output by character
 	
-	//MAX7219_Set_Digit(7, MAX7219_CHAR_L_BIG);    // "L"
-	//MAX7219_Set_Digit(6, MAX7219_CHAR_E_BIG);    // "E"
-	//MAX7219_Set_Digit(5, MAX7219_CHAR_D_SMALL);  // "d"
-	//MAX7219_Set_Digit(4, MAX7219_CHAR_SPACE);    // " "
-	//MAX7219_Set_Digit(3, MAX7219_CHAR_T_SMALL);  // "t"
-	//MAX7219_Set_Digit(2, MAX7219_CHAR_E_BIG);    // "E"
-	//MAX7219_Set_Digit(1, MAX7219_CHAR_S_BIG);    // "S"
-	//MAX7219_Set_Digit(0, MAX7219_CHAR_T_SMALL);  // "t"
+	//MAX7219_Set_Digit(7, MAX7219_L);      // "L"
+	//MAX7219_Set_Digit(6, MAX7219_E);      // "E"
+	//MAX7219_Set_Digit(5, MAX7219_d);      // "d"
+	//MAX7219_Set_Digit(4, MAX7219_SPACE);  // " "
+	//MAX7219_Set_Digit(3, MAX7219_t);      // "t"
+	//MAX7219_Set_Digit(2, MAX7219_E);      // "E"
+	//MAX7219_Set_Digit(1, MAX7219_S);      // "S"
+	//MAX7219_Set_Digit(0, MAX7219_t);      // "t"
 	
 	uint8_t max7219_data[] =
 	{
-		MAX7219_CHAR_L_BIG, MAX7219_CHAR_E_BIG, MAX7219_CHAR_D_SMALL,
+		MAX7219_L, MAX7219_E, MAX7219_d,
 		
-		MAX7219_CHAR_SPACE,
+		MAX7219_SPACE,
 		
-		MAX7219_CHAR_T_SMALL, MAX7219_CHAR_E_BIG, MAX7219_CHAR_S_BIG, MAX7219_CHAR_T_SMALL
+		MAX7219_t, MAX7219_E, MAX7219_S, MAX7219_t
 	};
 	
 	MAX7219_Set_Data(max7219_data, 8, true);
@@ -52,3 +50,5 @@ int main(void)
 	{
 	}
 }
+
+
