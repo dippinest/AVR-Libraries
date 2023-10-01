ee7392b2d200b8168bd68c153915a68513eb7e5e
Denis Chicherov <dippinest.workmail@gmail.com>
2023-10-01 12:55:34 +0300

    Update adc_configuration.h

--- AVR/Internal Peripherals and soft protocols/adc/adc_configuration.h
+++ AVR/Internal Peripherals and soft protocols/adc/adc_configuration.h
@@ -1,8 +1,12 @@
 #ifndef ADC_CONFIGURATION_H_
 #define ADC_CONFIGURATION_H_
 
-// if you don't want to use the built-in callback
-// to receive data, comment out #define
+
+// если вы хотите активировать асинхронный приём данных из АЦП
+// в буфер, раскомментируйте этот дефайн (иначе закомментируйте его)
+// -------------------------------------------------------------------------------
+// if you want to activate asynchronous data reception from the ADC
+// to the buffer, uncomment this define (otherwise comment it out)
 
 #define ADC_USE_CALLBACK
 
