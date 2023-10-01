1e0cbbcfdad57359db91932b9a73587388283b87
Denis Chicherov <dippinest.workmail@gmail.com>
2023-10-01 16:56:15 +0300

    Update ds1307_configuration.h

--- AVR/External Devices/DS1307/ds1307_configuration.h
+++ AVR/External Devices/DS1307/ds1307_configuration.h
@@ -2,7 +2,15 @@
 #ifndef DS1307_CONFIGURATION_H_
 #define DS1307_CONFIGURATION_H_
 
-// macro for using software I2C
+
+// если вы хотите работать с устройством посредством программного I2C,
+// раскомментируйте этот дефайн (иначе закомментируйте его)
+// -------------------------------------------------------------------------------
+// if you want to work with the device via software I2C,
+// uncomment this define (otherwise comment it out)
+
 #define DS1307_USE_SOFTI2C
 
 #endif
+
+
