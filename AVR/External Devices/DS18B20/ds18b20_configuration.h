e3b97291bf0c7e27980ca20d2991b1e0a7b741cc
Denis Chicherov <dippinest.workmail@gmail.com>
2023-10-01 17:10:31 +0300

    Update ds18b20_configuration.h

--- AVR/External Devices/DS18B20/ds18b20_configuration.h
+++ AVR/External Devices/DS18B20/ds18b20_configuration.h
@@ -2,6 +2,15 @@
 #ifndef DS18B20_CONFIGURATION_H_
 #define DS18B20_CONFIGURATION_H_
 
+
+// если вы хотите использовать несколько датчиков DS18B20,
+// раскомментируйте этот дефайн (иначе закомментируйте его)
+// -------------------------------------------------------------------------------
+// if you want to use multiple DS18B20 sensors, uncomment
+// this define (otherwise comment it out)
+
 //#define DS18B20_USE_MULTIPLE_SENSORS
 
 #endif
+
+
