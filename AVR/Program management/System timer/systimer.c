46dd9359d0d578daffc4e0d6085073c1815741aa
Denis Chicherov <dippinest.workmail@gmail.com>
2023-10-03 07:32:21 +0300

    Update systimer.c

--- AVR/Program management/System timer/systimer.c
+++ AVR/Program management/System timer/systimer.c
@@ -9,6 +9,28 @@ SYSTIMER_t SYSTIMER_Get_Value()
 	return __SYSTIMER;
 }
 
+// ===============================================================================
+
+
+SYSTIMER_Task_Params_t SYSTIMER_Get_Task_Params(SYSTIMER_t interval)
+{
+	SYSTIMER_Task_Params_t params;
+	
+	params.timer    = __SYSTIMER;
+	params.interval = interval;
+	
+	return params;
+}
+
+void SYSTIMER_Run_Task(SYSTIMER_Task_Params_t *task_params, void (*task)())
+{
+	if ((__SYSTIMER - task_params->timer) > task_params->interval)
+	{
+		task_params->timer = __SYSTIMER;
+		task();
+	}
+}
+
 
 // обработчик прерывания системного таймера
 //
