f45437acc65f247e6adb3d1d14f839576d3f0fda
Denis Chicherov <dippinest.workmail@gmail.com>
2023-11-04 19:37:15 +0300

    Update main.c

--- AVR/Program management/System timer/Examples/Measuring code execution time/main.c
+++ AVR/Program management/System timer/Examples/Measuring code execution time/main.c
@@ -8,24 +8,6 @@
 #include "uart.h"
 #include "systimer.h"
 
-
-// вы можете непосредственно использовать переменную системного таймера
-// __SYSTIMER, объявив её следующим образом, однако это опасно,
-// так как ей случайно можно присвоить другое значение, нарушив работу
-// других частей кода. Для безопасности вместо использования переменной
-// __SYSTIMER используйте функцию SYSTIMER_Get_Value()
-//
-// -------------------------------------------------------------------------------
-// you can directly use the system timer variable __SYSTIMER
-// by declaring it as follows, however, this is dangerous, since it
-// can accidentally be assigned a different value, disrupting the
-// operation of other parts of the code. For security, instead of
-// using the __SYSTIMER variable, use the
-// SYSTIMER_Get_Value() function
-//
-extern volatile SYSTIMER_t __SYSTIMER;
-
-
 int main(void)
 {
 	UART_Initialize(9600, true, false);
@@ -39,7 +21,7 @@ int main(void)
 	sei();
 	
 	
-	SYSTIMER_t start = __SYSTIMER; // SYSTIMER_t start = SYSTIMER_Get_Value();
+	SYSTIMER_t start = SYSTIMER_Get_Value();
 	
 	
 	// имитация долгих и сложных вычислений
@@ -51,7 +33,7 @@ int main(void)
 		asm("nop");
 	}
 	
-	SYSTIMER_t stop = __SYSTIMER; // SYSTIMER_t stop = SYSTIMER_Get_Value();
+	SYSTIMER_t stop = SYSTIMER_Get_Value();
 	
 	
 	UART_StringFmt_Transmit("Execution time = %d ticks\r\n", (stop - start));
