6770909700a054584f411dc94cd0db85e348f313
Denis Chicherov <dippinest.workmail@gmail.com>
2023-11-12 22:27:28 +0300

    Update delay_configuration.h

--- AVR/Program management/Delay/delay_configuration.h
+++ AVR/Program management/Delay/delay_configuration.h
@@ -3,8 +3,16 @@
 #define DELAY_CONFIGURATION_H_
 
 
-//#define DELAY_USE_YIELD_MS
-//#define DELAY_USE_YIELD_US
+// Если вы хотите задействовать возможность псевдопарралельного
+// выполнения отдельной задачи при выполнении задержки в основном
+// коде, раскомментируйте этот дефайн (иначе закомментируйте его)
+// -------------------------------------------------------------------------------
+// If you want to use the possibility of pseudo-parallel execution
+// of a separate task when performing a delay in the main code,
+// uncomment this define (otherwise comment it out)
+
+#define DELAY_MS_USE_YIELD
 
 
 #endif
+
