e246a2b961b4f52ea32446660f4b00e3a611205c
Denis Chicherov <dippinest.workmail@gmail.com>
2023-11-27 17:19:56 +0300

    Update onewire.h

--- AVR/Internal Peripherals and soft protocols/onewire/onewire.h
+++ AVR/Internal Peripherals and soft protocols/onewire/onewire.h
@@ -2,11 +2,15 @@
 
 // ===============================================================================
 //
-// Библиотека для работы с программным 1-Wire.
+// Библиотека для работы с программным 1-Wire. Библиотека позволяет работать
+// как с одиночным программным модулем 1-Wire, так и с их множеством (это настраивается
+// в файле onewire_configuration.h)
 //
 // -------------------------------------------------------------------------------
 //
-// Library for working with software 1-Wire.
+// Library for working with software 1-Wire. The library allows you to work with
+// a single 1-Wire software module, as well as with many of them (this is configured
+// in the onewire_configuration.h file)
 //
 // ===============================================================================
 
@@ -16,51 +20,87 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+
 #include <util/delay.h>
+
 #include <stdlib.h>
 #include <stdbool.h>
 
 #include "onewire_configuration.h"
 
+
 #define ONEWIRE_DEVICES_WAS_FOUND 0
 #define ONEWIRE_NO_DEVICES_FOUND  1
 
 
-// ===============================================================================
+
+#ifndef ONEWIRE_USE_MULTIPLE_INTERFACE
 
 
-inline void _ONEWIRE_Set_Low()
+inline void ONEWIRE_Set_Low()
 {
 	ONEWIRE_DDR |= (1 << ONEWIRE_PIN);
 }
 
-inline void _ONEWIRE_Set_High()
+inline void ONEWIRE_Set_High()
 {
 	ONEWIRE_DDR &= ~(1 << ONEWIRE_PIN);
 }
 
-inline uint8_t _ONEWIRE_Get_Level()
+inline uint8_t ONEWIRE_Get_Level()
 {
 	return ONEWIRE_PINX & (1 << ONEWIRE_PIN);
 }
 
 
+#else // ===============================================================================
+
+
+#ifndef T
+#define T(P) ((uint8_t*)&P)
+#endif
+
+
+typedef struct
+{
+	uint8_t *data_ddr;
+	uint8_t *data_pinx;
+	uint8_t  data_pin;
+	
+} ONEWIRE_t;
+
+
+
+ONEWIRE_t ONEWIRE_Get_Interface_Object(
+
+	uint8_t *data_ddr,
+	uint8_t *data_pinx,
+	uint8_t  data_pin
+);
+
+void ONEWIRE_Set_Target_Interface_Object(ONEWIRE_t *onewire_interface);
+
+volatile ONEWIRE_t *ONEWIRE_Get_Target_Interface_Object();
+
+
 // ===============================================================================
+
+
+void ONEWIRE_Set_Low();
+
+void ONEWIRE_Set_High();
+
+uint8_t ONEWIRE_Get_Level();
+
+
+#endif // ===============================================================================
 
 
 uint8_t ONEWIRE_Reset();
 
-
-// ===============================================================================
-
-
 void ONEWIRE_Send_Bit(uint8_t bit);
 
 void ONEWIRE_Send_Byte(uint8_t data);
-
-
-// ===============================================================================
-
 
 uint8_t ONEWIRE_Read_Bit();
 
@@ -70,17 +110,14 @@ uint8_t ONEWIRE_Read_Byte();
 // ===============================================================================
 
 
-void ONEWIRE_Apply_By_Unique_ROM_ID(uint8_t *unique_rom_id);
+void ONEWIRE_Apply_By_Unique_ROM_ID(uint8_t *_8byte_unique_rom_id_buf);
 
-uint8_t ONEWIRE_Get_Unique_ROM_ID(uint8_t *unique_rom_id);
-
-
-// ===============================================================================
-
+uint8_t ONEWIRE_Get_Unique_ROM_ID(uint8_t *_8byte_unique_rom_id_buf);
 
 uint8_t ONEWIRE_Get_CRC8(void *data, const uint8_t data_size);
 
-bool ONEWIRE_Check_CRC8_From_Unique_ROM_ID(uint8_t *unique_rom_id);
+bool ONEWIRE_Check_CRC8_From_Unique_ROM_ID(uint8_t *_8byte_unique_rom_id_buf);
+
 
 #endif
 
