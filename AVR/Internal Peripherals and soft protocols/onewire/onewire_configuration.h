d25e14df30b5d6f95e592452bb9b82dfeb991291
Denis Chicherov <dippinest.workmail@gmail.com>
2023-11-27 17:17:31 +0300

    Update onewire_configuration.h

--- AVR/Internal Peripherals and soft protocols/onewire/onewire_configuration.h
+++ AVR/Internal Peripherals and soft protocols/onewire/onewire_configuration.h
@@ -5,14 +5,29 @@
 #include <avr/io.h>
 
 
+// если вы хотите работать с несколькими программными 1-Wire, раскомментируйте
+// этот дефайн (иначе закомментируйте его)
+// -------------------------------------------------------------------------------
+// if you want to work with multiple software 1-Wire, uncomment this define
+// (otherwise comment it out)
+
+//#define ONEWIRE_USE_MULTIPLE_INTERFACE
+
+
+
+
+#ifndef ONEWIRE_USE_MULTIPLE_INTERFACE
+
 // определения GPIO для программного 1-Wire
 // -------------------------------------------------------------------------------
 // GPIO definitions for software 1-Wire
 
-#define ONEWIRE_DDR   DDRB
-#define ONEWIRE_PORT  PORTB
-#define ONEWIRE_PINX  PINB
-#define ONEWIRE_PIN   0
+#define ONEWIRE_DDR   DDRA
+#define ONEWIRE_PINX  PINA
+#define ONEWIRE_PIN   1
+
+#endif
+
 
 #endif
 
