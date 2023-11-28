0179cb30cff70f2d97530e6b0857f79215a0ea88
Denis Chicherov <dippinest.workmail@gmail.com>
2023-11-28 09:32:54 +0300

    Add files via upload

---
+++ AVR/External Devices/FRAM I2C/frami2c_configuration.h
@@ -0,0 +1,15 @@
+
+#ifndef FRAMI2C_CONFIGURATION_H_
+#define FRAMI2C_CONFIGURATION_H_
+
+
+// если вы хотите работать с устройством посредством программного I2C,
+// раскомментируйте этот дефайн (иначе закомментируйте его)
+// -------------------------------------------------------------------------------
+// if you want to work with the device via software I2C,
+// uncomment this define (otherwise comment it out)
+
+#define FRAMI2C_USE_SOFTI2C
+
+#endif
+
