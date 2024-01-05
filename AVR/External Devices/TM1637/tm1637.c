3bd66c27fe80c332a052490cb78d7d0aa44b7620
Denis Chicherov <dippinest.workmail@gmail.com>
2024-01-05 12:06:21 +0300

    Add files via upload

---
+++ AVR/External Devices/TM1637/tm1637.c
@@ -0,0 +1,406 @@
+
+
+#include "tm1637.h"
+
+
+static const uint8_t _digits[] =
+{
+	0b00111111, // 0
+	0b00000110, // 1
+	0b01011011, // 2
+	0b01001111, // 3
+	0b01100110, // 4
+	0b01101101, // 5
+	0b01111101, // 6
+	0b00000111, // 7
+	0b01111111, // 8
+	0b01101111, // 9
+};
+
+
+// ===============================================================================
+
+
+#define TM1637_DELAY_1  _delay_us(10)
+#define TM1637_DELAY_2  _delay_us(20)
+
+#define TM1637_DIO_SET_LOW	  TM1637_DIO_DDR |=  (1 << TM1637_DIO_PIN)
+#define TM1637_DIO_SET_HIGH   TM1637_DIO_DDR &= ~(1 << TM1637_DIO_PIN)
+
+#define TM1637_DIO_GET_VALUE (TM1637_DIO_PINX & (1 << TM1637_DIO_PIN))
+
+#define TM1637_SCL_SET_LOW	  TM1637_CLK_DDR |=  (1 << TM1637_CLK_PIN)
+#define TM1637_SCL_SET_HIGH   TM1637_CLK_DDR &= ~(1 << TM1637_CLK_PIN)
+
+
+static uint8_t _tm1637_configuration_register_buffer = 0b10000000;
+
+
+// ===============================================================================
+
+
+static inline void _TM1637_Start()
+{
+	TM1637_DIO_SET_LOW;
+	TM1637_DELAY_2;
+}
+
+static inline void _TM1637_Stop()
+{
+	TM1637_SCL_SET_LOW;
+	TM1637_DELAY_1;
+	
+	TM1637_DIO_SET_LOW;
+	TM1637_DELAY_1;
+	
+	TM1637_SCL_SET_HIGH;
+	TM1637_DELAY_2;
+	
+	TM1637_DIO_SET_HIGH;
+	TM1637_DELAY_2;
+}
+
+
+uint8_t _TM1637_Send_Byte(uint8_t byte)
+{
+	for (uint8_t i = 0; i < 8; ++i)
+	{
+		TM1637_SCL_SET_LOW;
+		TM1637_DELAY_1;
+		
+		if (byte & 0b1)
+		{
+			TM1637_DIO_SET_HIGH;
+		}
+		else
+		{
+			TM1637_DIO_SET_LOW;
+		}
+		
+		TM1637_DELAY_1;
+		
+		TM1637_SCL_SET_HIGH;
+		TM1637_DELAY_2;
+		
+		byte >>= 1;
+	}
+	
+	TM1637_SCL_SET_LOW;
+	TM1637_DIO_SET_HIGH;
+	TM1637_DELAY_2;
+	
+	TM1637_SCL_SET_HIGH;
+	TM1637_DELAY_1;
+
+	uint8_t ack = TM1637_DIO_GET_VALUE;
+	
+	if (ack == NACK)
+	{
+		TM1637_DIO_SET_LOW;
+	}
+	
+	TM1637_DELAY_1;
+
+	return ack;
+}
+
+
+// ===============================================================================
+
+
+void TM1637_Initialize(bool is_enable, uint8_t brightness_coef)
+{
+	TM1637_DIO_PORT &= ~(1 << TM1637_DIO_PIN);
+	TM1637_CLK_PORT &= ~(1 << TM1637_CLK_PIN);
+	
+	if (is_enable == true)
+	{
+		_tm1637_configuration_register_buffer |= (1 << 3);
+	}
+	
+	_tm1637_configuration_register_buffer |= (brightness_coef & 0b111);
+	
+	
+	_TM1637_Start();
+	
+	_TM1637_Send_Byte(_tm1637_configuration_register_buffer);
+	
+	_TM1637_Stop();
+}
+
+void TM1637_Set_Enable(bool is_enable)
+{
+	if (is_enable == true)
+	{
+		_tm1637_configuration_register_buffer |= (1 << 3);
+	}
+	else
+	{
+		_tm1637_configuration_register_buffer &= ~(1 << 3);
+	}
+	
+	
+	_TM1637_Start();
+	
+	_TM1637_Send_Byte(_tm1637_configuration_register_buffer);
+	
+	_TM1637_Stop();
+}
+
+void TM1637_Set_Brightness(uint8_t brightness_coef)
+{
+	_tm1637_configuration_register_buffer &= 0b11111000;
+	
+	_tm1637_configuration_register_buffer |= (brightness_coef & 0b111);
+	
+	
+	_TM1637_Start();
+	
+	_TM1637_Send_Byte(_tm1637_configuration_register_buffer);
+	
+	_TM1637_Stop();
+}
+
+
+void TM1637_Put_Symbols(uint8_t *simbols_arr, uint8_t simbols_arr_size)
+{
+	_TM1637_Start();
+	
+	_TM1637_Send_Byte(0b11000000);
+	
+	for (int8_t i = 0; i < simbols_arr_size; ++i)
+	{
+		_TM1637_Send_Byte(simbols_arr[i]);
+	}
+	
+	_TM1637_Stop();
+}
+
+
+void TM1637_Put_Symbols_Reverse(uint8_t *simbols_arr, uint8_t simbols_arr_size)
+{
+	_TM1637_Start();
+	
+	_TM1637_Send_Byte(0b11000000);
+	
+	for (int8_t i = 0; i < simbols_arr_size; ++i)
+	{
+		_TM1637_Send_Byte(simbols_arr[(simbols_arr_size - i - 1)]);
+	}
+	
+	_TM1637_Stop();
+}
+
+
+void TM1637_Put_Num_String(char *num_string, uint8_t num_string_size)
+{
+	uint8_t s = 0x00;
+	
+	
+	_TM1637_Start();
+	
+	_TM1637_Send_Byte(0b11000000);
+	
+	for (int8_t i = 0; i < num_string_size; ++i)
+	{
+		if (num_string[i] == ' ')
+		{
+			s = TM1637_EMPTY;
+		}
+		else if (num_string[i] == '-')
+		{
+			s = TM1637_MINUS;
+		}
+		else
+		{
+			uint8_t ch = num_string[i] - 48;
+			
+			if (ch < 10)
+			{
+				s = _digits[ch];
+			}
+			else
+			{
+				s = TM1637_EMPTY;
+			}
+		}
+		
+		_TM1637_Send_Byte(s);
+	}
+	
+	_TM1637_Stop();
+}
+
+
+void TM1637_Put_Num_String_Reverse(char *num_string, uint8_t num_string_size)
+{
+	uint8_t s = 0x00;
+	
+	
+	_TM1637_Start();
+	
+	_TM1637_Send_Byte(0b11000000);
+	
+	for (int8_t i = 0; i < num_string_size; ++i)
+	{
+		if (num_string[(num_string_size - i - 1)] == ' ')
+		{
+			s = TM1637_EMPTY;
+		}
+		else if (num_string[(num_string_size - i - 1)] == '-')
+		{
+			s = TM1637_MINUS;
+		}
+		else
+		{
+			uint8_t ch = num_string[(num_string_size - i - 1)] - 48;
+			
+			if (ch < 10)
+			{
+				s = _digits[ch];
+			}
+			else
+			{
+				s = TM1637_EMPTY;
+			}
+		}
+		
+		_TM1637_Send_Byte(s);
+	}
+	
+	_TM1637_Stop();
+}
+
+
+void TM1637_Clear()
+{
+	_TM1637_Start();
+	
+	_TM1637_Send_Byte(0b11000000);
+	
+	for (uint8_t i = 0; i < 4; ++i)
+	{
+		_TM1637_Send_Byte(TM1637_EMPTY);
+	}
+	
+	_TM1637_Stop();
+}
+
+
+char *TM1637_Convert_Num_String_To_Symbols_Array(char *num_string, uint8_t num_string_size)
+{
+	bool decimal_char_separator_flag = false;
+	uint8_t i = 0, j = 0, decimal_char_separator_pos = 0;
+	
+	while(i < num_string_size)
+	{
+		if (i != 0 && num_string[j] == '.')
+		{
+			decimal_char_separator_flag = true;
+			decimal_char_separator_pos  = i;
+			++j;
+		}
+		
+		if (num_string[j] == ' ')
+		{
+			num_string[i] = TM1637_EMPTY;
+		}
+		else if (num_string[j] == '-')
+		{
+			num_string[i] = TM1637_MINUS;
+		}
+		else
+		{
+			uint8_t ch = num_string[j] - 48;
+			
+			if (ch < 10)
+			{
+				num_string[i] = _digits[ch];
+			}
+			else
+			{
+				num_string[i] = TM1637_EMPTY;
+			}
+		}
+		
+		++i;
+		++j;
+	}
+	
+	if (decimal_char_separator_flag)
+	{
+		num_string[decimal_char_separator_pos - 1] |= 0b10000000;
+	}
+	
+	return num_string;
+}
+
+
+
+
+
+/*
+
+
+
+
+char *TM1637_Convert_Num_String_To_Symbols_Array(char *num_string, uint8_t num_string_size)
+{
+	bool decimal_char_separator_flag = false;
+	uint8_t i = 0, j = 0, decimal_char_separator_pos = 0;
+	
+	while(i < num_string_size)
+	{
+		if (i != 0 && num_string[j] == '.')
+		{
+			decimal_char_separator_flag = true;
+			decimal_char_separator_pos  = i;
+			++j;
+		}
+		
+		if (num_string[j] == ' ')
+		{
+			num_string[i] = TM1637_EMPTY;
+		}
+		else if (num_string[j] == '-')
+		{
+			num_string[i] = TM1637_MINUS;
+		}
+		else
+		{
+			uint8_t ch = num_string[j] - 48;
+			
+			if (ch < 10)
+			{
+				num_string[i] = _digits[ch];
+			}
+			else
+			{
+				num_string[i] = TM1637_EMPTY;
+			}
+		}
+		
+		++i;
+		++j;
+	}
+	
+	if (decimal_char_separator_flag)
+	{
+		num_string[decimal_char_separator_pos - 1] |= 0b10000000;
+	}
+	
+	return num_string;
+}
+
+
+
+
+*/
+
+
+
+
+
+
+
+
---
+++ AVR/External Devices/TM1637/tm1637.h
@@ -0,0 +1,118 @@
+
+
+// ===============================================================================
+//
+// Библиотека для работы с семисегментным дисплеем на TM1637
+//
+// -------------------------------------------------------------------------------
+//
+// Library for working with a seven-segment display on TM1637
+//
+// ===============================================================================
+
+
+#ifndef _TM1637_H_
+#define _TM1637_H_
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "tm1637_configuration.h"
+
+
+#ifndef ACK
+#define ACK true
+#endif
+
+#ifndef NACK
+#define NACK false
+#endif
+
+
+// ===============================================================================
+
+
+// macros for the luminance values of segments
+#define TM1637_BRIGHTNESS_COEFFICIENT_1   0x00
+#define TM1637_BRIGHTNESS_COEFFICIENT_2   0x01
+#define TM1637_BRIGHTNESS_COEFFICIENT_3   0x02
+#define TM1637_BRIGHTNESS_COEFFICIENT_4   0x03
+#define TM1637_BRIGHTNESS_COEFFICIENT_5   0x04
+#define TM1637_BRIGHTNESS_COEFFICIENT_6   0x05
+#define TM1637_BRIGHTNESS_COEFFICIENT_7   0x06
+#define TM1637_BRIGHTNESS_COEFFICIENT_8   0x07
+
+
+#define TM1637_LEDIND_SET_POINT(SYMBOL)   (SYMBOL |  (1 << 7))
+#define TM1637_LEDIND_RESET_POINT(SYMBOL) (SYMBOL & ~(1 << 7))
+
+
+#define TM1637_0      0b00111111  // '0'
+#define TM1637_1      0b00000110  // '1'
+#define TM1637_2      0b01011011  // '2'
+#define TM1637_3      0b01001111  // '3'
+#define TM1637_4      0b01100110  // '4'
+#define TM1637_5      0b01101101  // '5'
+#define TM1637_6      0b01111101  // '6'
+#define TM1637_7      0b00000111  // '7'
+#define TM1637_8      0b01111111  // '8'
+#define TM1637_9      0b01101111  // '9'
+#define TM1637_EMPTY  0b00000000  // ' '
+#define TM1637_MINUS  0b01000000  // '-'
+
+
+#define TM1637_A      0b01110111  // 'A'
+#define TM1637_B      0b01111100  // 'A'
+#define TM1637_C      0b00111001  // 'C'
+#define TM1637_D      0b01011110  // 'd'
+#define TM1637_E      0b01111001  // 'E'
+#define TM1637_F      0b01110001  // 'F'
+#define TM1637_G      0b01101111  // 'g -> like a 9'
+#define TM1637_H      0b01110100  // 'h'
+#define TM1637_I      0b00110000  // 'I'
+#define TM1637_J      0b00011110  // 'J'
+#define TM1637_K      0b01110110  // 'K -> like a H'
+#define TM1637_L      0b00111000  // 'L'
+#define TM1637_N      0b01010100  // 'n'
+#define TM1637_O      0b01011100  // 'o'
+#define TM1637_P      0b01110011  // 'P'
+#define TM1637_Q      0b01100111  // 'q'
+#define TM1637_R      0b01010000  // 'r'
+#define TM1637_S      0b01101101  // 'S -> like a 5'
+#define TM1637_T      0b01111000  // 't'
+#define TM1637_U      0b00111110  // 'U'
+#define TM1637_V      0b00011100  // 'v'
+#define TM1637_X      0b01110110  // 'X -> like a H'
+#define TM1637_Y      0b01101110  // 'Y'
+#define TM1637_Z      0b01011011  // 'Z -> like a 2'
+
+
+// ===============================================================================
+
+
+void TM1637_Initialize(bool is_enable, uint8_t brightness_coef);
+
+void TM1637_Set_Enable(bool is_enable);
+
+void TM1637_Set_Brightness(uint8_t brightness_coef);
+
+void TM1637_Put_Symbols(uint8_t *simbols_arr, uint8_t simbols_arr_size);
+
+void TM1637_Put_Symbols_Reverse(uint8_t *simbols_arr, uint8_t simbols_arr_size);
+
+void TM1637_Put_Num_String(char *num_string, uint8_t num_string_size);
+
+void TM1637_Put_Num_String_Reverse(char *num_string, uint8_t num_string_size);
+
+void TM1637_Clear();
+
+char *TM1637_Convert_Num_String_To_Symbols_Array(char *num_string, uint8_t num_string_size);
+
+
+#endif
+
+
