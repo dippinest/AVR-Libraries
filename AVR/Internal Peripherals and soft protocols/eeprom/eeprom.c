efdb3755016ede983733a7ec0fa934dfc780203c
Denis Chicherov <dippinest.workmail@gmail.com>
2025-01-04 08:28:11 +0300

    Update eeprom.c

--- AVR/Internal Peripherals and soft protocols/eeprom/eeprom.c
+++ AVR/Internal Peripherals and soft protocols/eeprom/eeprom.c
@@ -2,24 +2,42 @@
 #include "eeprom.h"
 
 
-void EEPROM_Write_Byte(uint16_t mem_addr, uint8_t byte)
+void EEPROM_Write_Byte(uint16_t mem_addr, uint8_t b)
 {
-	eeprom_write_byte((void*)mem_addr, byte);
+	while (EECR & (1 << EEWE));
+	
+	EEAR = mem_addr;
+	EEDR = b;
+	
+	EECR |= (1 << EEMWE);
+	EECR |= (1 << EEWE);
 }
 
 uint8_t EEPROM_Read_Byte(uint16_t mem_addr)
 {
-	return eeprom_read_byte((void*)mem_addr);
+	while (EECR & (1<<EEWE));
+	
+	EEAR = mem_addr;
+	
+	EECR |= (1 << EERE);
+	
+	return EEDR;
 }
 
 void EEPROM_Write_Data(uint16_t mem_addr, void *data, uint16_t data_size)
 {
-	eeprom_write_block(data, (void*)mem_addr, data_size);
+	for (uint16_t i = 0; i < data_size; ++i)
+	{
+		EEPROM_Write_Byte((mem_addr + i), ((uint8_t*)data)[i]);
+	}
 }
 
 void *EEPROM_Read_Data(uint16_t mem_addr, void *data, uint16_t data_size)
 {
-	eeprom_read_block(data, (void*)mem_addr, data_size);
+	for (uint16_t i = 0; i < data_size; ++i)
+	{
+		((uint8_t*)data)[i] = EEPROM_Read_Byte(mem_addr + i);
+	}
 	
 	return data;
 }
