cd61ce8a0f28e104156e9d7535352f7fbd142819
Denis Chicherov <dippinest.workmail@gmail.com>
2025-01-04 14:36:38 +0300

    Update eeprom_rand.c

--- AVR/Other/Internal EEPROM PRNG/eeprom_rand.c
+++ AVR/Other/Internal EEPROM PRNG/eeprom_rand.c
@@ -117,3 +117,53 @@ uint32_t EEPROM_RAND_Get_32Bit_Value()
 }
 
 
+// ===============================================================================
+
+
+bool EEPROM_RAND_Get_Bool_Value()
+{
+	return (bool)(EEPROM_RAND_Get_8Bit_Value() & 0x0001);
+}
+
+uint8_t EEPROM_RAND_Get_UInt8_Value(uint8_t min, uint8_t max)
+{
+	return min + (EEPROM_RAND_Get_8Bit_Value() % (max - min));
+}
+
+int8_t EEPROM_RAND_Get_Int8_Value(int8_t min, int8_t max)
+{
+	uint8_t r = min + (EEPROM_RAND_Get_8Bit_Value() % (max - min));
+
+	return (int8_t)r;
+}
+
+uint16_t EEPROM_RAND_Get_UInt16_Value(uint16_t min, uint16_t max)
+{
+	return min + (EEPROM_RAND_Get_16Bit_Value() % (max - min));
+}
+
+int16_t EEPROM_RAND_Get_Int16_Value(int16_t min, int16_t max)
+{
+	uint16_t r = min + (EEPROM_RAND_Get_16Bit_Value() % (max - min));
+
+	return (int16_t)r;
+}
+
+uint32_t EEPROM_RAND_Get_UInt32_Value(uint32_t min, uint32_t max)
+{
+	return min + (EEPROM_RAND_Get_32Bit_Value() % (max - min));
+}
+
+int32_t EEPROM_RAND_Get_Int32_Value(int32_t min, int32_t max)
+{
+	uint32_t r = min + (EEPROM_RAND_Get_32Bit_Value() % (max - min));
+
+	return (int32_t)r;
+}
+
+float EEPROM_RAND_Get_Float_Value()
+{
+	return (float)EEPROM_RAND_Get_32Bit_Value() / 4294967295ULL;
+}
+
+
