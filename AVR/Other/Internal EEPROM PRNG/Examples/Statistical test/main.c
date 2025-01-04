4a957f955ee81d5cdcedf949363016ad1d417d90
Denis Chicherov <dippinest.workmail@gmail.com>
2025-01-04 09:05:07 +0300

    Update main.c

--- AVR/Other/Internal EEPROM PRNG/Examples/Statistical test/main.c
+++ AVR/Other/Internal EEPROM PRNG/Examples/Statistical test/main.c
@@ -7,6 +7,17 @@ int main(void)
 	UART_Initialize(9600, true, false);
 	
 	
+	// Инициализация ГПСЧ. Адрес начальной рабочей ячейки - 0x0000.
+	// Диапазон рабочих адресов - от 0x0000 до 0x0020 (32 dec)
+	// -------------------------------------------------------------------------------
+	// Initialization of the PRNG. The address of the initial working cell is 0x0000.
+	// The range of working addresses is from 0x0000 to 0x0020 (32 dec)
+	EEPROM_RAND_Initialize(0x0000, 32);
+	
+	
+	// Количество сгенерированных чисел
+	// -------------------------------------------------------------------------------
+	// Number of generated numbers
 	#define NUM_OF_VAL 10000
 	
 	// генерирация 10.000 чисел в диапазоне от 0 до 256
@@ -14,12 +25,7 @@ int main(void)
 	// generating 10,000 numbers in the range from 0 to 256
 	for (uint16_t i = 0; i < NUM_OF_VAL; ++i)
 	{
-		// Генерирация 8-ми битного псевдослучайного значения с использованием
-		// диапазона рабочих адресов от 0x00 до 0x20. Основная ячейка - 0x00
-		// -------------------------------------------------------------------------------
-		// Generating an 8-bit pseudo-random value using a range of operating
-		// addresses from 0x00 to 0x20. The main cell is 0x00
-		uint8_t val = EEPROM_RAND_Get_8Bit_Value(0x00, 0x20);
+		uint8_t val = EEPROM_RAND_Get_8Bit_Value();
 		
 		UART_StringFmt_Transmit("%i, ", val);
 	}
