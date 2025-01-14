558dbe81189dd8e0bc165536cdf4baf4dec9fed3
Denis Chicherov <dippinest.workmail@gmail.com>
2025-01-14 18:47:44 +0300

    Update main.c

--- AVR/External Devices/HD44780 I2C/Examples/Flash strings/main.c
+++ AVR/External Devices/HD44780 I2C/Examples/Flash strings/main.c
@@ -13,9 +13,9 @@ int main(void)
 {
 	SOFTI2C_Initialize();
 	
-	HD44780_I2C_t display = HD44780_I2C_Get_Device_Object(HD44780_I2C_PCF8574_DEV_ADDR_A2T_A1T_A0T, true);
+	HD44780_I2C_t display = HD44780_I2C_Get_Object(HD44780_I2C_PCF8574_DEV_ADDR_A2T_A1T_A0T, true);
 	
-	HD44780_I2C_Set_Target_Device_Object(&display);
+	HD44780_I2C_Set_Target_Object(&display);
 	
 	
 	HD44780_I2C_Set_Cursor_Pos(0, 0);  HD44780_I2C_Print_Flash_String(FLASH_STRING_1);
