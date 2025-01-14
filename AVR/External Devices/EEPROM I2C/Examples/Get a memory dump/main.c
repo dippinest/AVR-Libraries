324cd31fda0253a5a84eb60b746d8a57a5dc8690
Denis Chicherov <dippinest.workmail@gmail.com>
2025-01-14 17:50:37 +0300

    Update main.c

--- AVR/External Devices/EEPROM I2C/Examples/Get a memory dump/main.c
+++ AVR/External Devices/EEPROM I2C/Examples/Get a memory dump/main.c
@@ -25,9 +25,9 @@ int main(void)
 	SOFTI2C_Initialize();
 	
 	
-	EEPROMI2C_t eeprom = EEPROMI2C_Get_Device_Object(EEPROM_CHIP_ADDR, EEPROM_MAX_MEM_ADDR, EEPROM_PAGE_SIZE);
+	EEPROMI2C_t eeprom = EEPROMI2C_Get_Object(EEPROM_CHIP_ADDR, EEPROM_MAX_MEM_ADDR, EEPROM_PAGE_SIZE);
 	
-	EEPROMI2C_Set_Target_Device_Object(&eeprom);
+	EEPROMI2C_Set_Target_Object(&eeprom);
 	
 	
 	UART_Initialize(9600, true, false);
@@ -53,7 +53,6 @@ int main(void)
 	{
 	}
 }
-
 
 
 
