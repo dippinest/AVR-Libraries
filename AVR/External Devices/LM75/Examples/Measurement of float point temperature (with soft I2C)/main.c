645ebd9aee4d10426fedc03e8eed672f1ac620ff
Denis Chicherov <dippinest.workmail@gmail.com>
2025-02-04 17:43:13 +0300

    Update main.c

--- AVR/External Devices/LM75/Examples/Measurement of float point temperature (with soft I2C)/main.c
+++ AVR/External Devices/LM75/Examples/Measurement of float point temperature (with soft I2C)/main.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	SOFTI2C_Initialize();
 	
-	HD44780_I2C_t display = HD44780_I2C_Get_Object(HD44780_I2C_PCF8574_DEV_ADDR_A2T_A1T_A0T, true);
+	HD44780_I2C_t display = HD44780_I2C_Create_Object(HD44780_I2C_PCF8574_DEV_ADDR_A2T_A1T_A0T, true);
 	HD44780_I2C_Set_Target_Object(&display);
 	
 	
-	LM75_t thermometer = LM75_Get_Object(LM75_I2C_DEV_ADDR_A2F_A1T_A0T, true);
+	LM75_t thermometer = LM75_Create_Object(LM75_I2C_DEV_ADDR_A2F_A1T_A0T, true);
 	LM75_Set_Target_Object(&thermometer);
 	
 	HD44780_I2C_Set_Cursor_Pos(0, 0); HD44780_I2C_Print_String("LM75 THERMOMETER");
