1beeaa62ba82fdfe9708f1fb3727a6f5b81153f0
Denis Chicherov <dippinest.workmail@gmail.com>
2025-03-13 11:12:10 +0300

    Update main.c

--- AVR/Internal Peripherals and soft protocols/IR Control/NEC Encoder/Example/IR Remote Control command encoder/main.c
+++ AVR/Internal Peripherals and soft protocols/IR Control/NEC Encoder/Example/IR Remote Control command encoder/main.c
@@ -11,12 +11,8 @@ int main(void)
 {
 	NEC_Encoder_Initialize();
 	
-	NEC_Encoder_FSM_Timer_Interrupt_Initialize();
-	
-	NEC_Encoder_Carrier_Frequency_Generator_Timer_Initialize();
 
 	sei();
-	
 	
 	uint8_t ir_nec_command = 0;
 	
