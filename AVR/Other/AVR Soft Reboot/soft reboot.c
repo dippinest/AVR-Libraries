2f64e95c37543eec5f78e3297f78d7ba8494f8d6
Denis Chicherov <dippinest.workmail@gmail.com>
2025-04-08 16:32:54 +0300

    Add files via upload

---
+++ AVR/Other/AVR Soft Reboot/soft reboot.c
@@ -0,0 +1,57 @@
+
+#include <avr/io.h>
+#include <avr/wdt.h>
+
+
+// функция программной перезагрузки микроконтроллера на основе watchdog таймера
+//
+// -------------------------------------------------------------------------------
+// function of software restart of the microcontroller based on the watchdog timer
+//
+void Soft_Reboot()
+{
+	// в этом месте микроконтроллер будет перезагружен через 15 мс
+	//
+	// -------------------------------------------------------------------------------
+	// at this point the microcontroller will reboot after 15 ms
+	//
+	wdt_enable(WDTO_15MS);
+	
+	while(1);
+}
+
+
+
+
+
+#include <util/delay.h>
+
+
+int main(void)
+{
+	// мигание светодиодом для наглядности
+	//
+	// -------------------------------------------------------------------------------
+	// LED flashing for clarity
+	//
+	DDRD = (1 << 7);
+	
+	PORTD |= (1 << 7);  _delay_ms(500);
+	PORTD &= ~(1 << 7); _delay_ms(500);
+	
+	
+	// вызов функции программной перезагрузки микроконтроллера
+	//
+	// -------------------------------------------------------------------------------
+	// calling the microcontroller's software reboot function
+	//
+	Soft_Reboot();
+	
+	
+	while (1)
+	{
+	}
+}
+
+
+
