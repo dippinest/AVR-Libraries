9fc50c2e00cac8cb873defcdd54ab5dffcb80282
Denis Chicherov <dippinest.workmail@gmail.com>
2025-04-18 08:41:38 +0300

    Update main.c

--- AVR/External Devices/TM1637/Examples/One TM1637/Float value/main.c
+++ AVR/External Devices/TM1637/Examples/One TM1637/Float value/main.c
@@ -1,7 +1,7 @@
 
 
 #include "tm1637.h"
-#include "itoa.h"
+#include "ftoa.h"
 
 
 static float data = -7.00;
@@ -14,14 +14,14 @@ int main(void)
 	TM1637_Initialize(true, TM1637_BRIGHTNESS_COEFFICIENT_2);
 	
 	
-	ITOA_Set_String_Buffer(buf);
+	FTOA_Set_String_Buffer(buf);
 	
 	while (1)
 	{
 		data += 0.1;
 		
-		TM1637_Convert_Num_String_To_Symbols_Array(ITOA_Float_To_String(data, 3, 1), 4);
-		TM1637_Put_Symbols((uint8_t*)ITOA_Get_String_Buffer_Ptr(), 4);
+		TM1637_Convert_Num_String_To_Symbols_Array(FTOA_Float32_To_String(data, 3, 1), 4);
+		TM1637_Put_Symbols((uint8_t*)FTOA_Get_String_Buffer_Ptr(), 4);
 		
 		_delay_ms(100);
 	}
