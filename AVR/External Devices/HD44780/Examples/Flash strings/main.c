0d0b1f29e3568571e163d8f7382fdf5089665f6f
Denis Chicherov <dippinest.workmail@gmail.com>
2025-04-18 08:44:50 +0300

    Update main.c

--- AVR/External Devices/HD44780/Examples/Flash strings/main.c
+++ AVR/External Devices/HD44780/Examples/Flash strings/main.c
@@ -1,19 +1,19 @@
 
 #include "hd44780.h"
 #include "deftypes.h"
-#include "itoa.h"
+#include "ftoa.h"
 
 AVR_FLASH_DATA(FLASH_STRING, char, "String FLASH");
 
 int main(void)
 {
-	ITOA_Set_String_Buffer_To_Dynamic_Memory(16);
+	FTOA_Set_String_Buffer_To_Dynamic_Memory(16);
 	
 	HD44780_Initialize(true);
 	
 	HD44780_Set_Cursor_Pos(0, 0);  HD44780_Print_Flash_String(FLASH_STRING); // write flash string
 	
-	HD44780_Set_Cursor_Pos(1, 0);  HD44780_Print_String("PI ="); HD44780_Print_String(ITOA_Float_To_String(MATH_PI, 2, 5));
+	HD44780_Set_Cursor_Pos(1, 0);  HD44780_Print_String("PI ="); HD44780_Print_String(FTOA_Float32_To_String(MATH_PI, 2, 5));
 	
 	while (1)
 	{
