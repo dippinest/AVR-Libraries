cf9ed94e9fb5b94d841abda410d051d7e77a7fcc
Denis Chicherov <dippinest.workmail@gmail.com>
2025-11-13 20:03:59 +0300

    Update ds1307.h

--- AVR/External Devices/DS1307/ds1307.h
+++ AVR/External Devices/DS1307/ds1307.h
@@ -18,41 +18,55 @@
 #ifndef DS1307_H_
 #define DS1307_H_
 
-#include <util/delay.h>
+
+#include <stdint.h>
 #include <stdbool.h>
-#include "bcdcode.h"
+
+
 
 #include "ds1307_configuration.h"
 
+
+
 // I2C address device
 #define _DS1307_ADDRESS_DEVICE 0x68
+
 
 // addresses of internal DS1307 registers (time data is stored in them)
 #define _DS1307_ADDR_REGISTER_SECOND                    0x00
 #define _DS1307_ADDR_REGISTER_MINUTES                   0x01
 #define _DS1307_ADDR_REGISTER_HOURS                     0x02
-#define _DS1307_ADDR_REGISTER_CURRENT_WEEKDAY           0x03
-#define _DS1307_ADDR_REGISTER_CURRENT_DAY_OF_MONTH      0x04
-#define _DS1307_ADDR_REGISTER_CURRENT_MONTH             0x05
-#define _DS1307_ADDR_REGISTER_CURRENT_YEAR              0x06
+#define _DS1307_ADDR_REGISTER_WEEKDAY                   0x03
+#define _DS1307_ADDR_REGISTER_DAY_OF_MONTH              0x04
+#define _DS1307_ADDR_REGISTER_MONTH                     0x05
+#define _DS1307_ADDR_REGISTER_YEAR                      0x06
 #define _DS1307_ADDR_CONTROL_REGISTER                   0x07
 #define _DS1307_ADDR_VERTEX_OF_USER_RAM_REGISTER        0x08
+
+
 
 #define _DS1307_HOUR_MODE_BIT                              6
 #define _DS1307_IS_HOUR_AM_BIT                             5
 #define _DS1307_WORK_PERMISSION_BIT_CH                     7
 #define _DS1307_SQUARE_WAVE_OUTPUT_PERMISSION_BIT_SQW      4
 
+
+
+// operating modes of the SQW generator
 #define DS1307_SQWE_FREQUENCY_1_HZ                       0x0
 #define DS1307_SQWE_FREQUENCY_4096_HZ                    0x1
 #define DS1307_SQWE_FREQUENCY_8192_HZ                    0x2
 #define DS1307_SQWE_FREQUENCY_32768_HZ                   0x3
 
 
+
+
 enum DS1307_Enum_Days
 {
 	MONDAY = 1, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
 };
+
+
 
 enum DS1307_Enum_Month
 {
@@ -74,21 +88,10 @@ typedef struct
 } DS1307_Data_t;
 
 
-void DS1307_Set_Seconds(uint8_t seconds);
 
-void DS1307_Set_Minutes(uint8_t minutes);
+// ===============================================================================
 
-void DS1307_Set_Hours(uint8_t hours);
 
-void DS1307_Set_Weekday(uint8_t weekday);
-
-void DS1307_Set_Day_Of_Month(uint8_t day_of_month);
-
-void DS1307_Set_Month(uint8_t month);
-
-void DS1307_Set_Year(uint8_t year);
-
-void DS1307_Set_Data_From_Struct(DS1307_Data_t *data);
 
 uint8_t DS1307_Get_Seconds();
 
@@ -104,15 +107,30 @@ uint8_t DS1307_Get_Month();
 
 uint8_t DS1307_Get_Year();
 
-void DS1307_Get_Data_To_Struct(DS1307_Data_t *data);
+
+
+// ===============================================================================
+
+
 
 void DS1307_Set_Enable(bool clock_is_enable);
 
 void DS1307_Set_Clock_Enable(bool clock_is_enable);
 
+
+
+// ===============================================================================
+
+
+
 void DS1307_Set_SQW_Enable(bool sqw_is_enable);
 
 void DS1307_Set_SQW_Frequency(uint8_t sqw_frequency);
+
+
+
+// ===============================================================================
+
 
 
 // these functions are used to write data to the user RAM area (56 bytes are available).
@@ -122,11 +140,10 @@ void DS1307_Write_Byte_To_User_RAM(uint8_t mem_addr, uint8_t byte);
 
 uint8_t DS1307_Read_Byte_From_User_RAM(uint8_t mem_addr);
 
-void DS1307_Write_Data_To_User_RAM(uint8_t mem_addr, void *data, uint8_t data_size);
-
-void *DS1307_Read_Data_From_User_RAM(uint8_t mem_addr, void *data, uint8_t data_size);
 
 
 #endif
+
+
 
 
