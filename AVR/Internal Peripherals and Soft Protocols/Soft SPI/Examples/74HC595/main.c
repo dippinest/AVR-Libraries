d32b2925378d939f9caeda5b99b692d4b45309e1
Denis Chicherov <dippinest.workmail@gmail.com>
2025-11-24 05:07:13 +0300

    Update main.c

--- AVR/Internal Peripherals and Soft Protocols/Soft SPI/Examples/74HC595/main.c
+++ AVR/Internal Peripherals and Soft Protocols/Soft SPI/Examples/74HC595/main.c
@@ -4,29 +4,39 @@
 #include "softspi.h"
 
 
-// macro enabling latches that transmit data to the storage register
-#define HC595_LATCHES         \
-	PORTD |=  (1 << PORTD7);  \
-	PORTD &= ~(1 << PORTD7);
+// функция для работы "защёлки" регистра
+//
+// ===============================================================================
+// function for register "latch" operation
+//
+inline void HC595_Latches()
+{
+	PORTD |=  (1 << 7);
+	
+	asm("NOP");
+	
+	PORTD &= ~(1 << 7);
+}
 
 int main(void)
 {
-	DDRD |= (1 << PORTD7);
+	DDRD |= (1 << 7);
 	
 	SOFTSPI_Initialize();
 	
-	// implementation of running lights
+
 	while (1)
 	{
 		for (int8_t i = 1; i < 8; ++i)
 		{
-			SOFTSPI_Send_Byte(~(1 << i)); HC595_LATCHES;
+			SOFTSPI_Send_Byte( ~(1 << i) ); HC595_Latches();
 			_delay_ms(100);
 		}
 		for (int8_t i = 6; i >= 0; --i)
 		{
-			SOFTSPI_Send_Byte(~(1 << i)); HC595_LATCHES;
+			SOFTSPI_Send_Byte( ~(1 << i) ); HC595_Latches();
 			_delay_ms(100);
 		}
 	}
 }
+
