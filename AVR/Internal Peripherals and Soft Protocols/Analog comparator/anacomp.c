6479cae8c46d635b08373d0975272d8ba3e140d7
Denis Chicherov <dippinest.workmail@gmail.com>
2025-12-01 19:37:43 +0300

    Update anacomp.c

--- AVR/Internal Peripherals and Soft Protocols/Analog comparator/anacomp.c
+++ AVR/Internal Peripherals and Soft Protocols/Analog comparator/anacomp.c
@@ -5,12 +5,12 @@
 
 static void (*_reception_callback)() = NULL;
 
-void ANACOMP_Set_CallBack_Function(void (*callback_function)())
+void ANACOMP_Set_Callback_Function(void (*callback_function)())
 {
 	_reception_callback = callback_function;
 }
 
-void *ANACOMP_Get_CallBack_Function()
+void *ANACOMP_Get_Callback_Function()
 {
 	return _reception_callback;
 }
