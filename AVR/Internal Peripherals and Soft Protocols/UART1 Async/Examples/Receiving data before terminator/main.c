6ce77aa838b9636b8a6c386f5d84382d5fb6cb64
Denis Chicherov <dippinest.workmail@gmail.com>
2025-12-01 19:32:19 +0300

    Update main.c

--- AVR/Internal Peripherals and Soft Protocols/UART1 Async/Examples/Receiving data before terminator/main.c
+++ AVR/Internal Peripherals and Soft Protocols/UART1 Async/Examples/Receiving data before terminator/main.c
@@ -31,7 +31,7 @@ int main(void)
 	UART1_Async_Set_Reception_Terminator(TERMINATOR_SYMBOL);
 	UART1_Async_Set_Reception_Terminator_Enable(true);
 	
-	UART1_Async_Set_Reception_CallBack_Function(send_buffer);
+	UART1_Async_Set_Reception_Callback_Function(send_buffer);
 	
 	UART1_Async_Start_Reception_Data_To_Buffer();
 	
@@ -42,6 +42,7 @@ int main(void)
 	{
 	}
 }
+
 
 
 
