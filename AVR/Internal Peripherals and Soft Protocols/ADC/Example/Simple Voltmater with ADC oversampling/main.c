be82c428ed2cc665ebfeea2537e9657eedf20af5
Denis Chicherov <dippinest.workmail@gmail.com>
2026-06-09 11:08:58 +0300

    Add files via upload

---
+++ AVR/Internal Peripherals and Soft Protocols/ADC/Example/Simple Voltmater with ADC oversampling/main.c
@@ -0,0 +1,29 @@
+
+
+#include "adc.h"
+#include "uart.h"
+
+#include <util/delay.h>
+
+
+
+int main(void)
+{
+	ADC_Initialize(0, ADC_PRESCALER_128, ADC_VREF_SOURCE_EXTERNAL_AVCC, true);
+	
+	UART_Initialize(9600, true, false);
+	
+	
+	
+	while (1)
+	{
+		float fvalue = ADC_Get_Voltage_Value_From_Measured_Value(ADC_Get_Oversampling_Value(12), 12);
+		
+		UART_StringFmt_Transmit("Voltage = %f\r\n", fvalue);
+		
+		
+		_delay_ms(250);
+	}
+}
+
+
