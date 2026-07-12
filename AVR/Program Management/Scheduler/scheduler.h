c8fe41e344368356873fa3d74e991e036544391d
Denis Chicherov <dippinest.workmail@gmail.com>
2026-07-12 09:16:07 +0300

    Update scheduler.h

--- AVR/Program Management/Scheduler/scheduler.h
+++ AVR/Program Management/Scheduler/scheduler.h
@@ -79,12 +79,16 @@ inline Scheduler_Mutex_t Scheduler_Create_Mutex()
 
 inline void Scheduler_Mutex_Set_Lock(Scheduler_Mutex_t *mutex)
 {
+  cli();
 	*mutex = SCHEDULER_MUTEX_LOCK;
+  sei();
 }
 
 inline void Scheduler_Mutex_Set_Unlock(Scheduler_Mutex_t *mutex)
 {
+  cli();
 	*mutex = SCHEDULER_MUTEX_UNLOCK;
+  sei();
 }
 
 inline bool Scheduler_Mutex_Is_Unlock(Scheduler_Mutex_t *mutex)
