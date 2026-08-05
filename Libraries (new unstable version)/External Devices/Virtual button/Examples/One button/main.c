e3748ec6f84e4856b8cd1efd7f3a4c18c497256d
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-05 07:59:13 +0300

    Add files via upload

---
+++ Libraries (new unstable version)/External Devices/Virtual button/Examples/One button/main.c
@@ -0,0 +1,135 @@
+
+
+#include <util/delay.h>
+
+#include "virtualbutton.h"
+#include "uart.h"
+
+
+
+VirtualButton_t vbutton;
+
+volatile uint8_t button_status = 0;
+
+
+
+
+
+// колбэк для обработки однократного нажатия кнопки
+// -------------------------------------------------------------------------------
+// callback for processing a single button press
+//
+void Press()
+{
+	UART_StringLn_Transmit("Button was pressed!");
+}
+
+// колбэк для обработки отжатия (отпускания) кнопки
+// -------------------------------------------------------------------------------
+// flask for processing the pressing (releasing) of the button
+//
+void Release()
+{
+	UART_StringLn_Transmit("Button was released!");
+}
+
+// колбэк для обработки нажатия кнопки через таймаут
+// -------------------------------------------------------------------------------
+// callback for processing button clicks via timeout
+//
+void Hold_With_Timeout()
+{
+	UART_StringLn_Transmit("Button is constantly being pressed...");
+}
+
+// колбэк для обработки долгого удержания кнопки
+// -------------------------------------------------------------------------------
+// callback for handling long button hold
+//
+void Long_Press()
+{
+	UART_StringLn_Transmit("Button is pressed very long...");
+}
+
+
+int main(void)
+{
+	DDRD &= ~(1 << 2);
+	
+	uint8_t button_status = 0;
+	
+	
+	VirtualButton_Initialize_Object(&vbutton, &button_status, 2, VIRTUALBUTTON_INPUT_LOGICAL_LEVEL_HIGH);
+	
+	
+	
+	UART_Initialize(9600, true, false);
+	
+	while (1)
+	{
+		// опрос логического уровня GPIO PD2
+		//
+		// -------------------------------------------------------------------------------
+		// GPIO PD2 logic level polling
+		//
+		if (PIND & (1 << 2))
+		{
+			button_status |=  (1 << 2);
+		}
+		else
+		{
+			button_status &= ~(1 << 2);
+		}
+		
+		
+		// функция опроса однократного нажатия и отпускания кнопки
+		//
+		// -------------------------------------------------------------------------------
+		// polling function of pressing and releasing the button once
+		//
+		VirtualButton_Pressed_And_Released_Polling(&vbutton, Press, Release);
+		
+		
+		// функция опроса нажатия кнопки через таймаут
+		//
+		// Второй параметр '10' является значением таймаута.
+		// При достижении 10-й итерации вызова этой функции сработает колбэк
+		// Hold_With_Timeout(), затем счётчик таймаута сбросится в 0
+		// -------------------------------------------------------------------------------
+		// function of polling button presses after a timeout
+		//
+		// The second parameter '10' is the timeout value.
+		// When the 10th iteration of calling this function is reached,
+		// the Hold_With_Timeout() callback will trigger, then the timeout counter
+		// will reset to 0
+		VirtualButton_Hold_With_Timeout_Polling(&vbutton, 10, Hold_With_Timeout);
+		
+		
+		// функция опроса длительного нажатия кнопки
+		//
+		// Второй параметр '50' является значением таймаута.
+		// При достижении 50-й итерации вызова этой функции сработает колбэк Long_Press().
+		// В отличии от VirtualButton_Hold_With_Timeout_Polling() этот обработчик
+		// выполняется однократно при зажатой
+		// -------------------------------------------------------------------------------
+		// long-press polling function
+		//
+		// The second parameter '50' is the timeout value.
+		// When the 50th iteration of calling this function is reached,
+		// the Long_Press() callback will trigger.
+		// Unlike VirtualButton_Hold_With_Timeout_Polling(),
+		// this handler is executed once when the button is clamped
+		VirtualButton_Long_Pressed_Polling(&vbutton, 50, Long_Press);
+		
+		
+		// вызываем функции опроса каждые 50 мс
+		// -------------------------------------------------------------------------------
+		// calling the polling functions every 50 ms
+		//
+		_delay_ms(50);
+	}
+}
+
+
+
+
---
+++ Libraries (new unstable version)/External Devices/Virtual button/virtualbutton.c
@@ -0,0 +1,267 @@
+
+
+#include "virtualbutton.h"
+
+
+
+void VirtualButton_Initialize_Object
+(
+	VirtualButton_t *vbutton,
+
+	uint8_t* virtual_port,
+	uint8_t  virtual_pin,
+
+	bool     pull
+)
+{
+	(*vbutton).virtual_port = virtual_port;
+	(*vbutton).virtual_pin  = virtual_pin;
+
+	(*vbutton).pull = pull;
+
+	(*vbutton).press_counter      = 0;
+	(*vbutton).long_press_counter = 0;
+}
+
+
+
+// ===============================================================================
+
+
+
+bool VirtualButton_Is_Pressed(VirtualButton_t* vbutton)
+{
+	if (vbutton->pull == VIRTUALBUTTON_INPUT_LOGICAL_LEVEL_HIGH)
+	{
+		if (_Bit_Is_Reset(*(vbutton->virtual_port), vbutton->virtual_pin))
+		{
+			return true;
+		}
+	}
+	else
+	{
+		if (_Bit_Is_Set(*(vbutton->virtual_port), vbutton->virtual_pin))
+		{
+			return true;
+		}
+	}
+	
+
+	return false;
+}
+
+
+void VirtualButton_Hold_Polling(VirtualButton_t* vbutton, void (*callback_function)())
+{
+	if (callback_function == NULL)
+	{
+		return;
+	}
+
+	if (VirtualButton_Is_Pressed(vbutton))
+	{
+		callback_function();
+	}
+}
+
+
+void VirtualButton_Hold_With_Timeout_Polling(VirtualButton_t* vbutton, uint16_t press_timeout, void (*callback_function)())
+{
+	if (callback_function == NULL)
+	{
+		return;
+	}
+
+
+	if (vbutton->pull == VIRTUALBUTTON_INPUT_LOGICAL_LEVEL_HIGH)
+	{
+		if (_Bit_Is_Reset(*(vbutton->virtual_port), vbutton->virtual_pin))
+		{
+			++(vbutton->press_counter);
+
+			if ((vbutton->press_counter > 0) && ((vbutton->press_counter % press_timeout) == 0))
+			{
+				callback_function();
+			}
+		}
+		else
+		{
+			vbutton->press_counter = 0;
+		}
+	}
+	else
+	{
+		if (_Bit_Is_Set(*(vbutton->virtual_port), vbutton->virtual_pin))
+		{
+			++(vbutton->press_counter);
+
+			if ((vbutton->press_counter > 0) && ((vbutton->press_counter % press_timeout) == 0))
+			{
+				callback_function();
+			}
+		}
+		else
+		{
+			vbutton->press_counter = 0;
+		}
+	}
+}
+
+
+void VirtualButton_Pressed_Polling(VirtualButton_t* vbutton, void (*callback_function)())
+{
+	if (callback_function == NULL)
+	{
+		return;
+	}
+
+
+	if (vbutton->pull == VIRTUALBUTTON_INPUT_LOGICAL_LEVEL_HIGH)
+	{
+		if (_Bit_Is_Reset(*(vbutton->virtual_port), vbutton->virtual_pin))
+		{
+			if (vbutton->press_counter == 0)
+			{
+				vbutton->press_counter = 1;
+
+				callback_function();
+			}
+		}
+		else
+		{
+			vbutton->press_counter = 0;
+		}
+	}
+	else
+	{
+		if (_Bit_Is_Set(*(vbutton->virtual_port), vbutton->virtual_pin))
+		{
+			if (vbutton->press_counter == 0)
+			{
+				vbutton->press_counter = 1;
+
+				callback_function();
+			}
+		}
+		else
+		{
+			vbutton->press_counter = 0;
+		}
+	}
+}
+
+
+void VirtualButton_Pressed_And_Released_Polling(VirtualButton_t* vbutton, void (*press_callback_function)(), void (*release_callback_function)())
+{
+	if (vbutton->pull == VIRTUALBUTTON_INPUT_LOGICAL_LEVEL_HIGH)
+	{
+		if (_Bit_Is_Reset(*(vbutton->virtual_port), vbutton->virtual_pin))
+		{
+			if (vbutton->press_counter == 0)
+			{
+				vbutton->press_counter = 1;
+
+				if (press_callback_function != NULL)
+				{
+					press_callback_function();
+				}
+			}
+		}
+		else
+		{
+			if (vbutton->press_counter)
+			{
+				if (release_callback_function != NULL)
+				{
+					release_callback_function();
+				}
+			}
+
+			vbutton->press_counter = 0;
+		}
+	}
+	else
+	{
+		if (_Bit_Is_Set(*(vbutton->virtual_port), vbutton->virtual_pin))
+		{
+			if (vbutton->press_counter == 0)
+			{
+				vbutton->press_counter = 1;
+
+				if (press_callback_function != NULL)
+				{
+					press_callback_function();
+				}
+			}
+		}
+		else
+		{
+			if (vbutton->press_counter)
+			{
+				if (release_callback_function != NULL)
+				{
+					release_callback_function();
+				}
+			}
+
+			vbutton->press_counter = 0;
+		}
+	}
+}
+
+
+void VirtualButton_Long_Pressed_Polling(VirtualButton_t* vbutton, uint16_t press_timeout, void (*callback_function)())
+{
+	if (callback_function == NULL)
+	{
+		return;
+	}
+	
+
+	if (vbutton->pull == VIRTUALBUTTON_INPUT_LOGICAL_LEVEL_HIGH)
+	{
+		if (_Bit_Is_Reset(*(vbutton->virtual_port), vbutton->virtual_pin))
+		{
+			if (vbutton->long_press_counter < press_timeout)
+			{
+				++(vbutton->long_press_counter);
+			}
+			else if (vbutton->long_press_counter == press_timeout)
+			{
+				callback_function();
+				++(vbutton->long_press_counter);
+			}
+		}
+		else
+		{
+			vbutton->long_press_counter = 0;
+		}
+	}
+	else
+	{
+		{
+			if (_Bit_Is_Set(*(vbutton->virtual_port), vbutton->virtual_pin))
+			{
+				if (vbutton->long_press_counter < press_timeout)
+				{
+					++(vbutton->long_press_counter);
+				}
+				else if (vbutton->long_press_counter == press_timeout)
+				{
+					callback_function();
+					++(vbutton->long_press_counter);
+				}
+			}
+			else
+			{
+				vbutton->long_press_counter = 0;
+			}
+		}
+	}
+}
+
+
+
+
+
+
