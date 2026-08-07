acb9cfcebdc656d324330a824c53892785fd1272
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-07 17:09:56 +0300

    Add files via upload

---
+++ Libraries (new unstable version)/External Devices/TM74HC595/Examples/HEX value/main.c
@@ -0,0 +1,99 @@
+
+
+#include <avr/interrupt.h>
+
+#include <util/delay.h>
+
+#include "softspi.h"
+#include "tm74hc595.h"
+#include "itohexa.h"
+
+
+
+// объект индикатора TM74HC595
+//
+// ===============================================================================
+// TM74HC595 indicator object
+//
+TM74HC595_t tm74hc595;
+
+
+
+
+// функция для работы "защёлки" регистра
+//
+// ===============================================================================
+// function for register "latch" operation
+//
+void TM74HC595_Latches()
+{
+	PORTA |=  (1 << 4);
+	
+	asm("NOP");
+	
+	PORTA &= ~(1 << 4);
+}
+
+
+
+
+
+uint16_t val = 0x0000;
+
+char string_buf[8];
+
+
+
+// обработчик обновления таймера
+// (для обслуживания динамической индикации таймера)
+//
+// ===============================================================================
+// timer update handler
+// (for maintenance of the dynamic timer display)
+//
+ISR(TIMER0_OVF_vect)
+{
+	TM74HC595_Put_Num_String(&tm74hc595, ITOHEXA_16bitNum_To_HexString(string_buf, val, true), 4);
+}
+
+
+int main(void)
+{
+	DDRA |= (1 << 4);
+	
+	
+	SOFTSPI_Initialize();
+	
+	
+	TM74HC595_Initialize_Object(&tm74hc595, TM74HC595_Latches);
+	
+
+	
+	// 2 ms = 500 Hz (for F_CPU = 16 MHz)
+	//
+	TIMSK = (1 << TOIE0);
+	TCNT0 = 255 - 250;
+	TCCR0 = (1 << CS02);
+	
+	
+	// не забудьте включить глобальные прерывания
+	//
+	// -------------------------------------------------------------------------------
+	// don't forget to enable global interrupts
+	//
+	sei();
+	
+	
+	while (1)
+	{
+		++val; _delay_ms(50);
+	}
+}
+
+
+
+
+
+
+
+
