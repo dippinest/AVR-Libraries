d956ba8b3fde495747c594f128853046615c2e09
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-11 16:50:01 +0300

    Update anacomp.c

--- Libraries (new unstable version)/Internal Peripherals and Soft Protocols/Analog comparator/anacomp.c
+++ Libraries (new unstable version)/Internal Peripherals and Soft Protocols/Analog comparator/anacomp.c
@@ -1,19 +1,24 @@
 
 #include "anacomp.h"
 
+
 #ifdef ANACOMP_USE_CALLBACK
 
 static void (*_reception_callback)() = NULL;
+
 
 void ANACOMP_Set_Callback_Function(void (*callback_function)())
 {
 	_reception_callback = callback_function;
 }
 
+
 void *ANACOMP_Get_Callback_Function()
 {
 	return _reception_callback;
 }
+
+
 
 ISR(ANA_COMP_vect)
 {
@@ -21,5 +26,7 @@ ISR(ANA_COMP_vect)
 }
 
 #endif
+
+
 
 
