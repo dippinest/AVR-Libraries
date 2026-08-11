41cf4a20937e188a94394f654125802abb42f07a
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-11 16:36:21 +0300

    Update main.c

--- Libraries (new unstable version)/External Devices/HD44780/Examples/Flash strings/main.c
+++ Libraries (new unstable version)/External Devices/HD44780/Examples/Flash strings/main.c
@@ -1,29 +1,37 @@
 
 
 #include "hd44780.h"
-#include "deftypes.h"
-#include "ftoa.h"
 
 
+AVR_FLASH_DATA(FLASH_STRING_1,    char, "String FLASH 1");
+AVR_FLASH_DATA(FLASH_STRING_2,    char, "String FLASH 2");
+AVR_FLASH_DATA(FLASH_FMT_STRING,  char, "Euler's num E = %.2f");
 
-
-AVR_FLASH_DATA(FLASH_STRING, char, "String FLASH");
 
 
 int main(void)
 {
-	FTOA_Set_String_Buffer_To_Dynamic_Memory(16);
-	
 	HD44780_Initialize(true);
 	
-	HD44780_Set_Cursor_Pos(0, 0);  HD44780_Print_Flash_String(FLASH_STRING); // write flash string
 	
-	HD44780_Set_Cursor_Pos(1, 0);  HD44780_Print_String("PI ="); HD44780_Print_String(FTOA_Float32_To_String(MATH_PI, 2, 5));
+	
+	HD44780_Set_Cursor_Pos(0, 0);  HD44780_Print_Flash_String(FLASH_STRING_1);
+	
+	HD44780_Set_Cursor_Pos(1, 0);  HD44780_Safe_Print_Flash_String(FLASH_STRING_2, 14);
+	
+	HD44780_Set_Cursor_Pos(2, 10); HD44780_Print_Flash_String_To_Char_Terminator(FLASH_STRING_2, 'g');
+	
+	
+	float Euler_number = 2.718281f;
+	
+	HD44780_Set_Cursor_Pos(3, 0);  HD44780_Print_Flash_StringFmt(FLASH_FMT_STRING, Euler_number); // not supported '%s' from flash!
 	
 	while (1)
 	{
 	}
 }
+
+
 
 
 
