50dacdc2e0e353cf3612c65b9ab94c472b4d3344
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-11 09:17:31 +0300

    Update lm75.c

--- Libraries (new unstable version)/External Devices/LM75/lm75.c
+++ Libraries (new unstable version)/External Devices/LM75/lm75.c
@@ -8,95 +8,74 @@
 
 
 
+#ifdef EEPROMI2C_USE_SOFTI2C
 
-#ifdef LM75_I2C_USE_SOFTI2C
 
 #include "softi2c.h"
 
-
-
-static void _LM75_Set_Register_Pointer(LM75_t *lm75, uint8_t addr_reg)
-{
-	SOFTI2C_Start();
-	SOFTI2C_Send_Byte(lm75->dev_addr << 1);
-	SOFTI2C_Send_Byte(addr_reg);
-	SOFTI2C_Stop();
-}
-
-
-static void _LM75_Write_UINT8_Configuration_Register(LM75_t *lm75)
-{
-	SOFTI2C_Start();
-	SOFTI2C_Send_Byte((lm75->dev_addr << 1) | 0);
-	SOFTI2C_Send_Byte(_LM75_CONFUGURATION_REGISTER_ADDR);
-	SOFTI2C_Send_Byte(lm75->configuration_register);
-	SOFTI2C_Stop();
-}
-
-
-static void _LM75_Write_UINT16_Register(LM75_t *lm75, uint8_t addr_reg, uint16_t data_register)
-{
-	SOFTI2C_Start();
-	SOFTI2C_Send_Byte((lm75->dev_addr << 1) | 0);
-	SOFTI2C_Send_Byte(addr_reg);
-	SOFTI2C_Send_Byte(data_register >> 8);
-	SOFTI2C_Send_Byte((uint8_t)data_register);
-	SOFTI2C_Stop();
-}
-
-
-static uint16_t _LM75_Read_UINT16_Register(LM75_t *lm75, uint8_t addr_reg)
-{
-	uint8_t high_byte = 0, low_byte = 0;
-	
-	
-	_LM75_Set_Register_Pointer(lm75, addr_reg);
-	
-	SOFTI2C_Start();
-	SOFTI2C_Send_Byte((lm75->dev_addr << 1) | 1);
-	SOFTI2C_Read_Byte(&high_byte, ACK);
-	SOFTI2C_Read_Byte(&low_byte, NACK);
-	SOFTI2C_Stop();
-	
-	return ((uint16_t)high_byte << 8) | low_byte;
-}
+#define _I2C_Start       SOFTI2C_Start
+#define _I2C_Send_Byte   SOFTI2C_Send_Byte
+#define _I2C_Restart     SOFTI2C_Restart
+#define _I2C_Read_Byte   SOFTI2C_Read_Byte
+#define _I2C_Stop        SOFTI2C_Stop
 
 
 
-#else // ===============================================================================
+#else
 
 
 
 #include "i2c.h"
 
+#define _I2C_Start       I2C_Start
+#define _I2C_Send_Byte   I2C_Send_Byte
+#define _I2C_Restart     I2C_Restart
+#define _I2C_Read_Byte   I2C_Read_Byte
+#define _I2C_Stop        I2C_Stop
+
+
+#endif
+
+
+
+// ===============================================================================
+
+
+
 
 static void _LM75_Set_Register_Pointer(LM75_t *lm75, uint8_t addr_reg)
 {
-	I2C_Start();
-	I2C_Send_Byte(lm75->dev_addr << 1);
-	I2C_Send_Byte(addr_reg);
-	I2C_Stop();
+	_I2C_Start();
+	
+	_I2C_Send_Byte(lm75->dev_addr << 1);
+	_I2C_Send_Byte(addr_reg);
+	
+	_I2C_Stop();
 }
 
 
 static void _LM75_Write_UINT8_Configuration_Register(LM75_t *lm75)
 {
-	I2C_Start();
-	I2C_Send_Byte((lm75->dev_addr << 1) | 0);
-	I2C_Send_Byte(_LM75_CONFUGURATION_REGISTER_ADDR);
-	I2C_Send_Byte(lm75->configuration_register);
-	I2C_Stop();
+	_I2C_Start();
+	
+	_I2C_Send_Byte((lm75->dev_addr << 1) | 0);
+	_I2C_Send_Byte(_LM75_CONFUGURATION_REGISTER_ADDR);
+	_I2C_Send_Byte(lm75->configuration_register);
+	
+	_I2C_Stop();
 }
 
 
 static void _LM75_Write_UINT16_Register(LM75_t *lm75, uint8_t addr_reg, uint16_t data_register)
 {
-	I2C_Start();
-	I2C_Send_Byte((lm75->dev_addr << 1) | 0);
-	I2C_Send_Byte(addr_reg);
-	I2C_Send_Byte(data_register >> 8);
-	I2C_Send_Byte((uint8_t)data_register);
-	I2C_Stop();
+	_I2C_Start();
+	
+	_I2C_Send_Byte((lm75->dev_addr << 1) | 0);
+	_I2C_Send_Byte(addr_reg);
+	_I2C_Send_Byte(data_register >> 8);
+	_I2C_Send_Byte((uint8_t)data_register);
+	
+	_I2C_Stop();
 }
 
 
@@ -106,22 +85,20 @@ static uint16_t _LM75_Read_UINT16_Register(LM75_t *lm75, uint8_t addr_reg)
 	
 	_LM75_Set_Register_Pointer(lm75, addr_reg);
 	
-	I2C_Start();
-	I2C_Send_Byte((lm75->dev_addr << 1) | 1);
-	I2C_Read_Byte(&high_byte, ACK);
-	I2C_Read_Byte(&low_byte, NACK);
-	I2C_Stop();
+	_I2C_Start();
+	
+	_I2C_Send_Byte((lm75->dev_addr << 1) | 1);
+	_I2C_Read_Byte(&high_byte, ACK);
+	_I2C_Read_Byte(&low_byte, NACK);
+	
+	_I2C_Stop();
+	
 	
 	return ((uint16_t)high_byte << 8) | low_byte;
 }
 
 
-
-
-#endif // ===============================================================================
-
-
-
+// ===============================================================================
 
 
 
@@ -165,6 +142,7 @@ void LM75_Set_Enable(LM75_t *lm75, bool thermometer_is_enable)
 }
 
 
+
 void LM75_Set_OS_Mode(LM75_t *lm75, bool os_is_comparator_mode)
 {
 	if (os_is_comparator_mode)
@@ -178,6 +156,7 @@ void LM75_Set_OS_Mode(LM75_t *lm75, bool os_is_comparator_mode)
 	
 	_LM75_Write_UINT8_Configuration_Register(lm75);
 }
+
 
 
 void LM75_Set_OS_Polarity(LM75_t *lm75, bool os_pol_is_low)
@@ -195,12 +174,14 @@ void LM75_Set_OS_Polarity(LM75_t *lm75, bool os_pol_is_low)
 }
 
 
+
 void LM75_Set_OS_Number_Of_Checks(LM75_t *lm75, uint8_t os_num_of_checks)
 {
 	lm75->configuration_register |= (os_num_of_checks << _LM75_CONFUGURATION_REGISTER_OS_F_QUE_BITS_POS);
 	
 	_LM75_Write_UINT8_Configuration_Register(lm75);
 }
+
 
 
 void LM75_Set_Integer_Upper_And_Lower_Limits_OS_Hysteresis_Temperature(LM75_t *lm75, int16_t low_limit_temp, int16_t high_limit_temp)
@@ -244,12 +225,14 @@ int16_t LM75_Get_Integer_Temperature(LM75_t *lm75)
 }
 
 
+
 float LM75_Get_Float_Temperature(LM75_t *lm75)
 {
 	int16_t raw_temperature_data = _LM75_Read_UINT16_Register(lm75, _LM75_DATA_TEMP_REGISTER_ADDR);
 	
 	return (float)(raw_temperature_data >> 5) * 0.125;
 }
+
 
 
 int32_t LM75_Get_FIXPoint_2_Fractional_Digits_Temperature(LM75_t *lm75)
@@ -275,8 +258,10 @@ int32_t LM75_Get_FIXPoint_2_Fractional_Digits_Temperature(LM75_t *lm75)
 		pfixp_temp = (int_part * 100) + (fract_part_3bit * 25);
 	}
 	
+	
 	return pfixp_temp;
 }
+
 
 
 int32_t LM75_Get_FIXPoint_3_Fractional_Digits_Temperature(LM75_t *lm75)
@@ -302,8 +287,10 @@ int32_t LM75_Get_FIXPoint_3_Fractional_Digits_Temperature(LM75_t *lm75)
 		pfixp_temp = (int_part * 1000) + (fract_part_3bit * 125);
 	}
 	
+	
 	return pfixp_temp;
 }
+
 
 
 
