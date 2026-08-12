0a987601955ae65a7499ff8007145f0c5ec4a5f0
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-12 09:29:43 +0300

    Update max7219_configuration.h

--- Libraries (new unstable version)/External Devices/MAX7219/max7219_configuration.h
+++ Libraries (new unstable version)/External Devices/MAX7219/max7219_configuration.h
@@ -1,6 +1,8 @@
 
+
 #ifndef MAX7219_CONFIGURATION_H_
 #define MAX7219_CONFIGURATION_H_
+
 
 
 // если вы хотите работать с устройством посредством программного SPI,
@@ -12,6 +14,7 @@
 #define MAX7219_USE_SOFTSPI
 
 
+
 // если вы хотите использовать колбэк-функции для изменения вывода
 // CS (chip select) SPI устройства, раскомментируйте этот дефайн (иначе закомментируйте его)
 // -------------------------------------------------------------------------------
@@ -20,10 +23,37 @@
 //
 #define MAX7219_USE_CS_CALLBACKS
 
+
+
+
+
+#ifdef MAX7219_USE_SOFTSPI
+
+#warning "COMPILER MESSAGE: Library "max7219.h" use software SPI!"
+
+#else
+
+#warning "COMPILER MESSAGE: Library "max7219.h" use hardware SPI!"
+
 #endif
 
 
 
+#ifdef MAX7219_USE_CS_CALLBACKS
+
+#warning "COMPILER MESSAGE: Library "max7219.h" use SPI CS callbacks!"
+
+#else
+
+#warning "COMPILER MESSAGE: Library "max7219.h" do not use SPI CS callbacks!"
+
+#endif
+
+
+
+
+
+#endif
 
 
 
