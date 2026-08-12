25feeb3bc25c0dd95e0615dab378f0822f29f327
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-12 08:19:03 +0300

    Update frami2c.c

--- Libraries (new unstable version)/External Devices/FRAM I2C/frami2c.c
+++ Libraries (new unstable version)/External Devices/FRAM I2C/frami2c.c
@@ -17,8 +17,6 @@
 #define _I2C_Read_Byte   SOFTI2C_Read_Byte
 #define _I2C_Stop        SOFTI2C_Stop
 
-#warning "COMPILER MESSAGE: Library "frami2c.h" use software I2C!"
-
 
 #else
 
@@ -30,8 +28,6 @@
 #define _I2C_Restart     I2C_Restart
 #define _I2C_Read_Byte   I2C_Read_Byte
 #define _I2C_Stop        I2C_Stop
-
-#warning "COMPILER MESSAGE: Library "frami2c.h" use hardware I2C!"
 
 #endif
 
