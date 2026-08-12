4140cfaa9f6588e359db46db762b2369320d14e8
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-12 08:22:12 +0300

    Update hd44780_i2c_configuration.h

--- Libraries (new unstable version)/External Devices/HD44780 I2C/hd44780_i2c_configuration.h
+++ Libraries (new unstable version)/External Devices/HD44780 I2C/hd44780_i2c_configuration.h
@@ -11,13 +11,22 @@
 
 #define HD44780_I2C_USE_SOFTI2C
 
+
+
+
+
+#ifdef HD44780_I2C_USE_SOFTI2C
+
+#warning "COMPILER MESSAGE: Library "hd44780_i2c.h" use software I2C!"
+
+#else
+
+#warning "COMPILER MESSAGE: Library "hd44780_i2c.h" use hardware I2C!"
+
 #endif
 
 
 
-
-
-
-
+#endif
 
 
