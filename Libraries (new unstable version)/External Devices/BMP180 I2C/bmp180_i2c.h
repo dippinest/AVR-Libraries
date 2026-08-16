5627054a870694a258f08c40b7bf770cc73f3bcf
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-16 19:16:35 +0300

    Update bmp180_i2c.h

--- Libraries (new unstable version)/External Devices/BMP180 I2C/bmp180_i2c.h
+++ Libraries (new unstable version)/External Devices/BMP180 I2C/bmp180_i2c.h
@@ -129,7 +129,13 @@ int16_t BMP180_I2C_Get_Temperature(BMP180_I2C_t *bmp180_i2c);
 
 int32_t BMP180_I2C_Get_Pressure(BMP180_I2C_t *bmp180_i2c, const BMP180_I2C_PRESSURE_OSS_ACCURACY pressure_oss_accuracy_mode);
 
-void BMP180_I2C_Get_Temperature_And_Pressure(BMP180_I2C_t *bmp180_i2c, int16_t *temperature, int32_t *pressure, const BMP180_I2C_PRESSURE_OSS_ACCURACY pressure_oss_accuracy_mode);
+void BMP180_I2C_Get_Temperature_And_Pressure
+(
+	BMP180_I2C_t *bmp180_i2c, 
+	int16_t *temperature,
+	int32_t *pressure,
+	const BMP180_I2C_PRESSURE_OSS_ACCURACY pressure_oss_accuracy_mode
+);
 
 
 
