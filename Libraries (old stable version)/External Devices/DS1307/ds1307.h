be5522178980648f3769d95f472fc44199454749
Denis Chicherov <dippinest.workmail@gmail.com>
2026-08-18 07:54:55 +0300

    Update ds1307.h

--- Libraries (old stable version)/External Devices/DS1307/ds1307.h
+++ Libraries (old stable version)/External Devices/DS1307/ds1307.h
@@ -50,26 +50,61 @@
 
 
 
+// ===============================================================================
+
+
+
 // operating modes of the SQW generator
-#define DS1307_SQWE_FREQUENCY_1_HZ                       0x0
-#define DS1307_SQWE_FREQUENCY_4096_HZ                    0x1
-#define DS1307_SQWE_FREQUENCY_8192_HZ                    0x2
-#define DS1307_SQWE_FREQUENCY_32768_HZ                   0x3
-
-
-
-
-enum DS1307_Enum_Days
+typedef enum
 {
-	MONDAY = 1, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+	DS1307_SQWE_FREQUENCY_1_HZ     = 0x0,
+	DS1307_SQWE_FREQUENCY_4096_HZ  = 0x1,
+	DS1307_SQWE_FREQUENCY_8192_HZ  = 0x2,
+	DS1307_SQWE_FREQUENCY_32768_HZ = 0x4
+	
+} DS1307_SQWE_FREQUENCY;
+
+
+#define DS1307_USER_RAM_MAX_ADDRESS                      0x37
+
+
+
+// ===============================================================================
+
+
+
+enum DS1307_DAYS
+{
+	MONDAY    = 1,
+	TUESDAY   = 2, 
+	WEDNESDAY = 3,
+	THURSDAY  = 4,
+	FRIDAY    = 5,
+	SATURDAY  = 6,
+	SUNDAY    = 7
 };
 
 
 
-enum DS1307_Enum_Month
+enum DS1307_MONTH
 {
-	JANUARY = 1, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
+	JANUARY   = 1,
+	FEBRUARY  = 2,
+	MARCH     = 3,
+	APRIL     = 4,
+	MAY       = 5,
+	JUNE      = 6,
+	JULY      = 7,
+	AUGUST    = 8,
+	SEPTEMBER = 9,
+	OCTOBER   = 10,
+	NOVEMBER  = 11,
+	DECEMBER  = 12
 };
+
+
+// ===============================================================================
+
 
 
 // structure for storing time data
@@ -123,7 +158,17 @@ void DS1307_Set_Clock_Enable(bool clock_is_enable);
 
 void DS1307_Set_SQW_Enable(bool sqw_is_enable);
 
-void DS1307_Set_SQW_Frequency(uint8_t sqw_frequency);
+void DS1307_Set_SQW_Frequency(DS1307_SQWE_FREQUENCY sqw_frequency);
+
+
+
+// ===============================================================================
+
+
+
+void DS1307_Set_Data_From_Struct(DS1307_Data_t *data);
+
+void DS1307_Get_Data_To_Struct(DS1307_Data_t *data);
 
 
 
@@ -134,9 +179,9 @@ void DS1307_Set_SQW_Frequency(uint8_t sqw_frequency);
 // these functions are used to write data to the user RAM area (56 bytes are available).
 // Memory addresses range - from 0x00 to 0x37
 
-void DS1307_Write_Byte_To_User_RAM(uint8_t mem_addr, uint8_t byte);
+void DS1307_Write_Byte_To_User_56Byte_RAM(uint8_t mem_addr, uint8_t byte);
 
-uint8_t DS1307_Read_Byte_From_User_RAM(uint8_t mem_addr);
+uint8_t DS1307_Read_Byte_From_User_56Byte_RAM(uint8_t mem_addr);
 
 
 
